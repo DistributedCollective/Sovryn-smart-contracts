18f603f4425dafdf8878891d6f1a752bcd4d1181
ororopickpocket <ororopickpocket@protonmail.com>
2020-09-25 12:26:15 +0000

    updated sweap test to not mint tokens anymore

--- scripts/swap_test.py
+++ scripts/swap_test.py
@@ -100,10 +100,10 @@ def test_margin_trading_sending_collateral_tokens():
     loan_token_sent = 100e18
     leverage_amount = 2e18
 
-    SUSD.mint(loan_token.address,loan_token_sent*6)
+    #SUSD.mint(loan_token.address,loan_token_sent*6)
     # address loanToken, address collateralToken, uint256 newPrincipal,uint256 marginAmount, bool isTorqueLoan
     collateral_token_sent = sovryn.getRequiredCollateral(SUSD.address,RBTC.address,loan_token_sent*2,50e18, False)
-    RBTC.mint(acct,collateral_token_sent)
+    #RBTC.mint(acct,collateral_token_sent)
     # important! WEth is being held by the loanToken contract itself, all other tokens are transfered directly from
     # the sender and need approval
     RBTC.approve(loan_token.address, collateral_token_sent)
@@ -128,8 +128,8 @@ def test_margin_trading_sending_loan_tokens():
     loan_token_sent = 100e18
     leverage_amount = 2e18
 
-    SUSD.mint(loan_token.address, loan_token_sent*3)
-    SUSD.mint(acct, loan_token_sent)
+    #SUSD.mint(loan_token.address, loan_token_sent*3)
+    #SUSD.mint(acct, loan_token_sent)
     SUSD.approve(loan_token.address, loan_token_sent)
 
     tx = loan_token.marginTrade(
