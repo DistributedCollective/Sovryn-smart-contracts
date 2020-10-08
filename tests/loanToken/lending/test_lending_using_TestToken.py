65054762819d0fe725b22af63925d52b7a0dcfbd
ororopickpocket <ororopickpocket@protonmail.com>
2020-10-08 10:43:18 +0000

    test profitOf

--- tests/loanToken/lending/test_lending_using_TestToken.py
+++ tests/loanToken/lending/test_lending_using_TestToken.py
@@ -3,6 +3,7 @@ test script for testing the loan token lending logic with 2 TestTokens.
 1. Lending / Minting
 2. Cashing Out / Burning
 3. Cashing out / Burning more than available
+4. profitOf function
 '''
 
 #!/usr/bin/python3
@@ -32,3 +33,28 @@ try to burn more than I'm possessing. should burn the maximum possible amount.
 def test_cash_out_from_the_pool_more_of_lender_balance_should_not_fail(loanToken, accounts, SUSD):
     cash_out_from_the_pool_more_of_lender_balance_should_not_fail(loanToken, accounts, SUSD)
 
+'''
+tests if profitOf is showing the current profit (opposed to total profit)
+'''
+def test_profit(loanToken, accounts, SUSD, RBTC, chain, set_demand_curve, sovryn):
+    lend_to_the_pool(loanToken, accounts, SUSD, RBTC, chain, set_demand_curve, sovryn)
+    #above functionn also opens a trading position, so I need to add some more funds to be able to withdraw everything
+    balanceOf0 = loanToken.assetBalanceOf(accounts[0])
+    SUSD.approve(loanToken.address, balanceOf0)
+    loanToken.mint(accounts[2], balanceOf0)
+    profitBefore = loanToken.profitOf(accounts[0])
+    iTokenBalance = loanToken.balanceOf(accounts[0])
+    
+    #burn everything -> profit should be 0
+    loanToken.burn(accounts[0], iTokenBalance)
+    profitInt = loanToken.profitOf(accounts[0])
+    
+    #lend again and wait some time -> profit should rise again, but less than before, because there are more funds in the pool.
+    SUSD.approve(loanToken.address, balanceOf0+100e18)
+    loanToken.mint(accounts[0], balanceOf0)
+    SUSD.approve(loanToken.address, balanceOf0+100e18)
+    chain.sleep(100)
+    chain.mine(1)
+
+    assert(profitInt == 0)
+    assert(profitAfter > 0 and profitAfter < profitBefore))
