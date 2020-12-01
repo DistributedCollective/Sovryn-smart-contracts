42b157a928c05e94be4894767d9d92fd79070843
James Howlett <jameshowlett977@gmail.com>
2020-12-01 16:51:52 +0200

    updated RSOV

--- contracts/governance/Vesting/RSOV.sol
+++ contracts/governance/Vesting/RSOV.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.5.17;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "../Staking/Staking.sol";
+import "../Staking/SafeMath96.sol";
 
-//TODO Ownable ?
-contract RSOV is ERC20, ERC20Detailed, Ownable, SafeMath96 {
+/**
+* Sovryn Reward Token
+*/
+contract RSOV is ERC20, ERC20Detailed, SafeMath96 {
     
     string constant NAME = "Sovryn Reward Token";
     string constant SYMBOL = "RSOV";
@@ -15,18 +16,25 @@ contract RSOV is ERC20, ERC20Detailed, Ownable, SafeMath96 {
     
     ///@notice the SOV token contract
     IERC20 public SOV;
-    ///@notice the staking contract address
-    Staking public staking;
+    ///@notice the staking contract
+    IStaking public staking;
     
     //TODO should we use constants ?
 //    constructor(
 //        string memory _name,
 //        string memory _symbol,
-//        uint8 _decimals
+//        uint8 _decimals,
+//        address _SOV,
+//        address _staking
 //    )
 //        ERC20Detailed(_name, _symbol, _decimals)
 //        public
 //    {
+//        require(_SOV != address(0), "SOV address invalid");
+//        require(_staking != address(0), "staking address invalid");
+//
+//        SOV = IERC20(_SOV);
+//        staking = IStaking(_staking);
 //    }
     
     constructor(
@@ -36,14 +44,16 @@ contract RSOV is ERC20, ERC20Detailed, Ownable, SafeMath96 {
         ERC20Detailed(NAME, SYMBOL, DECIMALS)
         public
     {
-        require(_SOV != address(0), "SOV address invalid");
-        require(_staking != address(0), "staking address invalid");
+        require(_SOV != address(0), "RSOV::SOV address invalid");
+        require(_staking != address(0), "RSOV::staking address invalid");
     
         SOV = IERC20(_SOV);
-        staking = Staking(_staking);
+        staking = IStaking(_staking);
     }
     
     function mint(uint256 _amount) public {
+        require(_amount > 0, "RSOV::mint: amount invalid");
+        
         //holds SOV
         bool success = SOV.transferFrom(msg.sender, address(this), _amount);
         require(success);
@@ -53,15 +63,22 @@ contract RSOV is ERC20, ERC20Detailed, Ownable, SafeMath96 {
     }
     
     function burn(uint256 _amount) public {
-        _amount = safe96(_amount, "RSOV::burn: amount exceeds 96 bits");
+        require(_amount > 0, "RSOV:: burn: amount invalid");
+        uint96 amount96 = safe96(_amount, "RSOV::burn: amount exceeds 96 bits");
+        
         //burns RSOV tokens
         _burn(msg.sender, _amount);
         
         //stakes SOV tokens in the user's behalf
-        SOV.approve(address(staking), amount);
+        SOV.approve(address(staking), _amount);
         //TODO staking duration, should it be possible to update ?
         uint until = block.timestamp + staking.MAX_DURATION();
-        staking.stake(_amount, until, msg.sender, msg.sender);
+        staking.stake(amount96, until, msg.sender, msg.sender);
     }
 
+}
+
+interface IStaking {
+    function stake(uint96 amount, uint until, address stakeFor, address delegatee) external;
+    function MAX_DURATION() external returns (uint);
 }
