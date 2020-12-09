0462f737bb6faae9374de2a686e6cb2ec4e225cb
ororopickpocket <ororopickpocket@protonmail.com>
2020-12-09 14:48:05 +0000

    added function to setup loan params for multiple collateral addresses

--- scripts/contractInteraction/contract_interaction.py
+++ scripts/contractInteraction/contract_interaction.py
@@ -6,6 +6,7 @@ This script serves the purpose of interacting with existing smart contracts on t
 from brownie import *
 from brownie.network.contract import InterfaceContainer
 import json
+import copy
 
 def main():
     
@@ -35,8 +36,14 @@ def main():
     #print('new LoanTokenLogicStandard contract for iWRBTC:' + logicContract.address)
     #replaceLoanTokenLogic(contracts['iRBTC'], logicContract.address)
     
+    #setupLoanParamsForCollaterals(contracts['iBPro'], [contracts['DoC'], contracts['USDT']])
+    #setupLoanParamsForCollaterals(contracts['iDOC'], [contracts['BPro'], contracts['USDT']])
+    #setupLoanParamsForCollaterals(contracts['iUSDT'], [contracts['DoC'], contracts['BPro']])
+    #setupLoanParamsForCollaterals(contracts['iRBTC'], [contracts['BPro'], contracts['USDT']])
 
-    deployMultisig(['0xdB3DB4c5695f2aab0F406C86d1f35D326685d055', '0x5092019A3E0334586273A21a701F1BD859ECAbD6', '0xD6da34D91fC59134A132b17e7b5a472CA1BeA794'], 2)
+    #deployMultisig(['0xdB3DB4c5695f2aab0F406C86d1f35D326685d055', '0x5092019A3E0334586273A21a701F1BD859ECAbD6', '0xD6da34D91fC59134A132b17e7b5a472CA1BeA794'], 2)
+    #deployMultisig(['0x2bD2201bfe156a71EB0d02837172FFc237218505', acct, '0xdB3DB4c5695f2aab0F406C86d1f35D326685d055', '0x5092019A3E0334586273A21a701F1BD859ECAbD6', '0xD6da34D91fC59134A132b17e7b5a472CA1BeA794'], 2)
+
     
 def loadConfig():
     global contracts, acct
@@ -45,7 +52,7 @@ def loadConfig():
         configFile =  open('./scripts/contractInteraction/mainnet_contracts.json')
     elif this_network == "testnet":
         configFile =  open('./scripts/contractInteraction/testnet_contracts.json')
-    contracts = json.load(configFile)
+    #contracts = json.load(configFile)
     acct = accounts.load("rskdeployer")
     
 
@@ -377,3 +384,30 @@ def addLiquidity(converter, reserve, amount):
 def deployMultisig(owners, requiredConf):
      multisig = acct.deploy(MultiSigWallet, owners, requiredConf)
      print("multisig:", multisig)
+     
+def setupLoanParamsForCollaterals(loanTokenAddress, collateralAddresses):
+    loanToken = Contract.from_abi("loanToken", address=loanTokenAddress, abi=LoanTokenLogicStandard.abi, owner=acct)
+    marginParams = []
+    torqueParams = []
+    for collateralAddress in collateralAddresses:
+        marginData = [
+            b"0x0", ## id
+            False, ## active
+            str(acct), ## owner
+            "0x0000000000000000000000000000000000000000", ## loanToken -> will be overwritten
+            collateralAddress, ## collateralToken.
+            Wei("20 ether"), ## minInitialMargin -> 20% (allows up to 5x leverage)
+            Wei("15 ether"), ## maintenanceMargin -> 15%, below liquidation
+            0 ## fixedLoanTerm -> will be overwritten with 28 days
+        ]
+        torqueData = copy.deepcopy(marginData)
+        torqueData[5] = Wei("50 ether")
+        print(torqueData)
+        
+        marginParams.append(marginData)
+        torqueParams.append(torqueData)
+
+    #configure the token settings, and set the setting contract address at the loan token logic contract
+    tx = loanToken.setupLoanParams(marginParams, False)
+    tx = loanToken.setupLoanParams(torqueParams, True)
+    
