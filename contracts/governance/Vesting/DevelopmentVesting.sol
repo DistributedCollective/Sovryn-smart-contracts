b11e1f8e0810fb4d6503ac83b3810cfcc5fe17fb
James Howlett <jameshowlett977@gmail.com>
2020-12-17 19:36:26 +0200

    updated DevelopmentVesting

--- contracts/governance/Vesting/DevelopmentVesting.sol
+++ contracts/governance/Vesting/DevelopmentVesting.sol
@@ -16,24 +16,24 @@ contract DevelopmentVesting is Ownable {
     uint public cliff;
     ///@notice the duration. after this period all tokens will have been unlocked
     uint public duration;
+    ///@notice the frequency. part of tokens will have been unlocked periodically
+    uint public frequency;
     ///@notice the start date of the vesting
     uint public startDate;
     ///@notice the end date of the vesting
     uint public endDate;
-    ///@notice constant used for computing the vesting dates
-    //TODO should be configurable ? wait for an answer
-    //uint public intervalLength;
-    uint constant FOUR_WEEKS = 4 weeks;
     ///@notice amount of vested tokens
     uint public amount;
     ///@notice amount of already withdrawn tokens
     uint public withdrawnAmount;
-    ///@notice amount of already transferred locked tokens
-    uint public transferredLockedAmount;
 
-    event TokensStaked(address indexed caller, uint amount);
-    event TokensWithdrawn(address indexed caller, address receiver, uint amount);
-    event LockedTokensTransferred(address indexed caller, address vesting, uint amount);
+    ///@notice amount of locked tokens, these tokens can be withdrawn any time by an owner
+    uint public lockedAmount;
+
+    event TokensSent(address indexed caller, uint amount);
+    event LockedTokensWithdrawn(address indexed caller, address receiver, uint amount);
+    event TokensVested(address indexed caller, uint amount);
+    event VestedTokensWithdrawn(address indexed caller, address receiver, uint amount);
 
     /**
      * @dev Throws if called by any account other than the token owner or the contract owner.
@@ -46,49 +46,76 @@ contract DevelopmentVesting is Ownable {
     /**
      * @notice setup the vesting schedule
      * @param _SOV the SOV token address
+     * @param _tokenOwner the owner of the tokens
      * @param _cliff the cliff in seconds
      * @param _duration the total duration in seconds
+     * @param _frequency the withdrawal interval in seconds
      */
-    constructor(address _SOV, address _tokenOwner, uint _cliff, uint _duration) public {
+    constructor(address _SOV, address _tokenOwner, uint _cliff, uint _duration, uint _frequency) public {
         require(_SOV != address(0), "SOV address invalid");
         require(_tokenOwner != address(0), "token owner address invalid");
-        _validateSchedule(_cliff, _duration);
+
+        _setSchedule(_cliff, _duration, _frequency);
 
         SOV = IERC20(_SOV);
         tokenOwner = _tokenOwner;
-        cliff = _cliff;
-        duration = _duration;
     }
 
-    //TODO add setTokenOwner
+    function setTokenOwner(address _tokenOwner) public onlyOwner {
+        require(_tokenOwner != address(0), "token owner address invalid");
+        tokenOwner = _tokenOwner;
+    }
+
+    function sendTokens(uint _amount) public {
+        require(_amount > 0, "amount needs to be bigger than 0");
+
+        //retrieve the SOV tokens
+        bool success = SOV.transferFrom(msg.sender, address(this), _amount);
+        require(success);
+
+        lockedAmount += _amount;
+
+        emit TokensSent(msg.sender, _amount);
+    }
+
+    //TODO onlyOwners or onlyOwner ?
+    function withdrawLockedTokens(uint _amount, address _receiver) public onlyOwners {
+        require(_amount > 0, "amount needs to be bigger than 0");
+        require(_amount <= lockedAmount, "amount is not available");
+        require(_receiver != address(0), "receiver address invalid");
+
+        bool success = SOV.transfer(_receiver, _amount);
+        require(success);
+
+        lockedAmount -= _amount;
+
+        emit LockedTokensWithdrawn(msg.sender, _receiver, _amount);
+    }
 
     /**
      * @notice change the vesting schedule
      * @param _cliff the cliff in seconds
      * @param _duration the total duration in seconds
+     * @param _frequency the withdrawal interval in seconds
      */
-    function changeSchedule(uint _cliff, uint _duration) public onlyOwner {
-        _validateSchedule(_cliff, _duration);
+    function changeSchedule(uint _cliff, uint _duration, uint _frequency) public onlyOwner {
+        _setSchedule(_cliff, _duration, _frequency);
+    }
+
+    function _setSchedule(uint _cliff, uint _duration, uint _frequency) internal {
+        require(_duration >= _cliff, "duration must be bigger than or equal to the cliff");
+        require(_duration - cliff / _frequency >= 1, "frequency is bigger than (duration - cliff)");
 
         cliff = _cliff;
         duration = _duration;
+        frequency = _frequency;
     }
-
-    function _validateSchedule(uint _cliff, uint _duration) internal pure {
-        require(_duration >= _cliff, "duration must be bigger than or equal to the cliff");
-        //TODO add checkings
-
-    }
-
-    //TODO add sendTokens - wait for an answer
-//    function sendTokens
 
     /**
      * @notice stakes tokens
      * @param _amount the amount of tokens to stake
      */
     function vestTokens(uint _amount) public {
-        //TODO is it possible to vest N times ? - wait for an answer
         require(startDate == 0);
         startDate = block.timestamp;
         endDate = block.timestamp + duration;
@@ -97,20 +124,17 @@ contract DevelopmentVesting is Ownable {
         bool success = SOV.transferFrom(msg.sender, address(this), _amount);
         require(success);
 
-        //increase balance
-//        amount = amount.add(_amount);
         amount = _amount;
 
-        emit TokensStaked(msg.sender, amount);
+        emit TokensVested(msg.sender, amount);
     }
 
-    //TODO withdrawTokens - onlyOwners - the governance should be able to spend the unlocked tokens
     /**
      * @notice withdraws unlocked tokens and forwards them to an address specified by the token owner
      * @param _receiver the receiving address
      * @dev this operation can be done N times
      */
-    function withdrawTokens(address _receiver) public onlyOwners {
+    function withdrawVestedTokens(address _receiver) public onlyOwners {
         require(_receiver != address(0), "receiver address invalid");
 
         uint availableAmount = _getAvailableAmount();
@@ -120,33 +144,21 @@ contract DevelopmentVesting is Ownable {
         bool success = SOV.transfer(_receiver, availableAmount);
         require(success);
 
-        emit TokensWithdrawn(msg.sender, _receiver, availableAmount);
+        emit VestedTokensWithdrawn(msg.sender, _receiver, availableAmount);
     }
 
-    //TODO use this calculation in tests
-    //100 - amount
-    //80 - unlocked
-    //20 - withdrawn
-    //60 - available
-    //50 - transferred
-    //50 - available
     function _getAvailableAmount() internal view returns (uint) {
-        uint availableAmount = _getUnlockedAmount() - withdrawnAmount;
-        if (availableAmount > amount - transferredLockedAmount) {
-            availableAmount = amount - transferredLockedAmount;
-        }
-        return availableAmount;
+        return _getUnlockedAmount() - withdrawnAmount;
     }
 
     //amount of SOV tokens never hits 2**256 - 1
     function _getUnlockedAmount() internal view returns (uint) {
         uint start = startDate + cliff;
         uint end = endDate;
-        uint intervalLength = FOUR_WEEKS;
         if (block.timestamp >= start) {
-            uint numIntervals = (end - start) / intervalLength + 1;
+            uint numIntervals = (end - start) / frequency + 1;
             uint amountPerInterval = amount / numIntervals;
-            uint256 intervalNumber = (block.timestamp - start) / intervalLength + 1;
+            uint256 intervalNumber = (block.timestamp - start) / frequency + 1;
             if (intervalNumber > numIntervals) {
                 intervalNumber = numIntervals;
             }
