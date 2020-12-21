a72ca95d51c57239a3a06ff58733f1320a935193
James Howlett <jameshowlett977@gmail.com>
2020-12-21 20:16:15 +0200

    removed openzeppelin-solidity (fixed brownie config)

--- contracts/governance/Vesting/RSOV.sol
+++ contracts/governance/Vesting/RSOV.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.17;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../../openzeppelin/ERC20Detailed.sol";
+import "../../openzeppelin/IERC20_.sol";
+import "../../openzeppelin/ERC20.sol";
+import "../../openzeppelin/Ownable.sol";
 import "../Staking/SafeMath96.sol";
 import "../Staking/IStaking.sol";
 
@@ -25,7 +25,7 @@ contract RSOV is ERC20, ERC20Detailed, Ownable, SafeMath96 {
     uint96 constant DIRECT_TRANSFER_PART = 14;
 
     ///@notice the SOV token contract
-    IERC20 public SOV;
+    IERC20_ public SOV;
     ///@notice the staking contract
     IStaking public staking;
 
@@ -47,7 +47,7 @@ contract RSOV is ERC20, ERC20Detailed, Ownable, SafeMath96 {
         require(_SOV != address(0), "RSOV::SOV address invalid");
         require(_staking != address(0), "RSOV::staking address invalid");
     
-        SOV = IERC20(_SOV);
+        SOV = IERC20_(_SOV);
         staking = IStaking(_staking);
     }
     
