5c8a8a110276acad9d01a199d6d98b25b47ccec5
James Howlett <jameshowlett977@gmail.com>
2020-12-24 11:10:41 +0200

    governance deployment: added fee sharing, read contracts from config

--- scripts/deployment/deploy_governance.py
+++ scripts/deployment/deploy_governance.py
@@ -1,6 +1,7 @@
 from brownie import *
 
-import time;
+import time
+import json
 
 def main():
     thisNetwork = network.show_active()
@@ -11,30 +12,45 @@ def main():
 
     adminQuorumVotes = 4
     adminMinPercentageVotes = 50
-    
+
     if thisNetwork == "development":
         acct = accounts[0]
+        # configFile =  open('./scripts/contractInteraction/testnet_contracts.json')
         guardian = acct
         SOV = acct.deploy(TestToken, "SOV", "SOV", 18, 1e26).address
         delay = 2*24*60*60
     elif thisNetwork == "testnet":
         acct = accounts.load("rskdeployer")
-        guardian = '0x189ecD23E9e34CFC07bFC3b7f5711A23F43F8a57'
+        configFile =  open('./scripts/contractInteraction/testnet_contracts.json')
         SOV = '0x04fa98E97A376a086e3BcAB99c076CB249e5740D'
         delay = 3*60*60
     elif thisNetwork == "rsk-mainnet":
         acct = accounts.load("rskdeployer")
+        configFile =  open('./scripts/contractInteraction/mainnet_contracts.json')
         delay = 2*24*60*60
         raise Exception("set guardian and SOV token!")
     else:
         raise Exception("network not supported")
-    
-    
+
+    # load deployed contracts addresses
+    contracts = json.load(configFile)
+    protocolAddress = contracts['sovrynProtocol']
+    if (thisNetwork == "testnet" or thisNetwork == "rsk-mainnet"):
+        guardian = contracts['multisig']
+
     #deploy the staking contracts
     stakingLogic = acct.deploy(Staking)
     staking = acct.deploy(StakingProxy, SOV)
     staking.setImplementation(stakingLogic.address)
+    staking = Contract.from_abi("Staking", address=staking.address, abi=Staking.abi, owner=acct)
 
+    #deploy fee sharing contract
+    feeSharing = acct.deploy(FeeSharingProxy, protocolAddress, staking.address, staking.address) #TODO _loanToken
+
+    # set fee sharing
+    staking.setFeeSharing(feeSharing.address)
+
+    # [timelockOwner]
     #params: owner, delay
     timelockOwner = acct.deploy(Timelock, acct, delay)
     #params: timelockOwner. staking, guardian
@@ -47,7 +63,7 @@ def main():
     print("schedule ownership(admin) transfer for ", eta)
     timelockOwner.queueTransaction(timelockOwner.address, 0, "setPendingAdmin(address)", dataString[10:], eta)
 
-
+    # [timelockAdmin]
     #params: admin, delay
     timelockAdmin = acct.deploy(Timelock, acct, delay)
     #params: timelockAdmin. staking, guardian
@@ -59,5 +75,3 @@ def main():
     eta = round(time.time()) + delay + 300
     print("schedule ownership(admin) transfer for ", eta)
     timelockAdmin.queueTransaction(timelockAdmin.address, 0, "setPendingAdmin(address)", dataString[10:], eta)
-
-    #@todo need someone to update deploy, idk python, can miss something    
