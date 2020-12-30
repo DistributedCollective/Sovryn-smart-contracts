63b28bfa95d2afa71d909ad4312695b3a6c92454
James Howlett <jameshowlett977@gmail.com>
2020-12-30 14:44:17 +0200

    updated MultiSigKeyHolders

--- contracts/multisig/MultiSigKeyHolders.sol
+++ contracts/multisig/MultiSigKeyHolders.sol
@@ -5,40 +5,55 @@ import "../openzeppelin/Ownable.sol";
 
 contract MultiSigKeyHolders is Ownable {
 
+    string private constant ERROR_INVALID_ADDRESS = "Invalid address";
+    string private constant ERROR_ADDRESS_ALREADY_ADDED = "Address already added";
+    string private constant ERROR_ADDRESS_NOT_FOUND = "Address not found";
 
-    function addEthereumAddress(address account) public onlyOwner {
+    mapping (address => bool) private isEthereumAddressAdded;
+    address[] private ethereumAddresses;
+
+    mapping (string => bool) private isBitcoinAddressAdded;
+    string[] private bitcoinAddresses;
+
+    function addEthereumAddress(address _address) public onlyOwner {
+        require(_address != address(0), ERROR_INVALID_ADDRESS);
+        require(!isEthereumAddressAdded[_address], ERROR_ADDRESS_ALREADY_ADDED);
+
 
     }
 
-    function removeEthereumAddress(address account) public onlyOwner {
+    function removeEthereumAddress(address _address) public onlyOwner {
+        require(_address != address(0), ERROR_INVALID_ADDRESS);
+        require(isEthereumAddressAdded[_address], ERROR_ADDRESS_NOT_FOUND);
 
     }
 
-    function isEthereumAddressAdded(address account) public returns (bool) {
+    function isEthereumAddressOwner(address _address) public view returns (bool) {
+        return isEthereumAddressAdded[_address];
+    }
+
+    function getEthereumAddresses() public view returns (address[] memory) {
+        return ethereumAddresses;
+    }
+
+    function addBitcoinAddress(string memory _address) public onlyOwner {
+        require(bytes(_address).length != 0, ERROR_INVALID_ADDRESS);
+        require(!isBitcoinAddressAdded[_address], ERROR_ADDRESS_ALREADY_ADDED);
 
     }
 
-    function getEthereumAddresses() public returns (address[] memory) {
+    function removeBitcoinAddress(string memory _address) public onlyOwner {
+        require(bytes(_address).length != 0, ERROR_INVALID_ADDRESS);
+        require(!isBitcoinAddressAdded[_address], ERROR_ADDRESS_NOT_FOUND);
 
     }
 
-
-//    function addBitcoinPublicKey(string memory account) public onlyOwner {
-    function addBitcoinAddress(address account) public onlyOwner {
-
+    function isBitcoinAddressOwner(string memory _address) public view returns (bool) {
+        return isBitcoinAddressAdded[_address];
     }
 
-    function removeBitcoinAddress(string memory account) public onlyOwner {
-
+    function getBitcoinAddresses() public view returns (string[] memory) {
+        return bitcoinAddresses;
     }
-
-    function isBitcoinAddressAdded(string memory account) public returns (bool) {
-
-    }
-
-    function getBitcoinAddresses() public returns (string[] memory) {
-
-    }
-
 
 }
