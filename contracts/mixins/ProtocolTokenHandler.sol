b47c39ed8ae152dfc01004ae33696ba6e33220cf
wukong-xxs <wukong.xxs@gmail.com>
2021-01-04 17:03:59 +0800

    update smart contract and test script

--- contracts/mixins/ProtocolTokenHandler.sol
+++ contracts/mixins/ProtocolTokenHandler.sol
@@ -3,13 +3,11 @@ pragma experimental ABIEncoderV2;
 
 import "../interfaces/IERC20.sol";
 import "../openzeppelin/Address.sol";
-import "../openzeppelin/ECDSA.sol";
 import "../openzeppelin/SafeERC20.sol";
 import "../openzeppelin/Ownable.sol";
 
 contract ProtocolTokenHandler is Ownable {
     using Address for address;
-    using ECDSA for bytes32;
     using SafeERC20 for IERC20;
 
     address public protocolTokenAddress;
@@ -17,8 +15,8 @@ contract ProtocolTokenHandler is Ownable {
     mapping (address => bool) public isSigner;
     mapping (address => uint256) public userNonce;
 
-    event AddSigner(address indexed singer);
-    event RemoveSigner(address indexed singer);
+    event AddSigner(address indexed signer);
+    event RemoveSigner(address indexed signer);
     event SetRequiredCount(uint256 indexed requiredCount);
     event Deposit(address indexed sender, uint256 indexed amount);
     event Withdraw(address indexed caller, address indexed recipient, uint256 indexed amount);
@@ -79,41 +77,50 @@ contract ProtocolTokenHandler is Ownable {
     }
 
     /**
-     * the message/data from users to be signed by authorized signers, should include in order: 
-     * address of recipient, addresss of destination contract (should be this contract address), amount of tokens to be withdrawed, nounce
      * @param _signs array includes signs from authorized signers, amount/length should equal to requiredCount
+     * @param _recipient address of recipient
+     * @param _contractAddress addresss of destination contract (should be this contract address)
+     * @param _amount amount of tokens to be withdrawed
+     * @param _nonce nonce to avoid replay attacks
+     * @param _signs array includes signs from authorized signers
      */
     function withdraw(address _recipient, address _contractAddress, uint256 _amount, uint256 _nonce, bytes[] memory _signs) public {
         require(_contractAddress == address(this), "contract address disagreement");
         require(_amount > 0, "amount should larger than zero");
+
         uint256 oldNonce = userNonce[msg.sender];
         require(_nonce > oldNonce, "nonce smaller than last know nonce");
         userNonce[msg.sender] = _nonce;
 
-        bytes32 _hash = keccak256(abi.encodePacked(_recipient, _contractAddress, _amount, _nonce)).toEthSignedMessageHash();
- 
-
-        bytes[] memory signs = new bytes[](requiredCount);
-        signs = _signs;
-        address[] memory verifiedSigners = new address[](requiredCount);
+        bytes32 _hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(_recipient, _contractAddress, _amount, _nonce))));
+        
         uint8 v;
         bytes32 r;
         bytes32 s;
-        bytes memory sign;
         address signer;
+        bytes memory sign;
+        bytes[] memory signs = new bytes[](requiredCount);
+        address[] memory verifiedSigners = new address[](requiredCount);
+
+        signs = _signs;
+
         for (uint256 i = 0; i < requiredCount; i++) {
             sign = signs[i];
             require(sign.length == 65, "length of sign is wrong");
+
             assembly {
-                r := mload(add(sign, 32))
-                s := mload(add(sign, 64))
-                v := byte(0, mload(add(sign, 96)))
+                r := mload(add(sign, 0x20))
+                s := mload(add(sign, 0x40))
+                v := add(0x1b, byte(0, mload(add(sign, 0x60))))
             }
+
             signer = ecrecover(_hash, v, r, s);
             require(isSigner[signer], "signer not authorized");
+
             for (uint256 q = 0; q < i; q++) {
                 require(signer != verifiedSigners[q], "signer verified");
             }
+            
             verifiedSigners[i] = signer;
         }
 
