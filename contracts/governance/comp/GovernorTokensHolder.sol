38782a6e8202eed018cbcfe39a2dbea28dcb9f25
James Howlett <jameshowlett977@gmail.com>
2021-01-06 10:23:41 +0200

    fixes for GovernorTokensHolder

--- contracts/governance/comp/GovernorTokensHolder.sol
+++ contracts/governance/comp/GovernorTokensHolder.sol
@@ -5,10 +5,10 @@ import "../../interfaces/IERC20.sol";
 
 contract GovernorTokensHolder is Ownable {
 
-    IERC20 token;
+    IERC20 public token;
 
     constructor(address _token) public {
-        token = token;
+        token = IERC20(_token);
     }
 
     function transfer(address recipient, uint256 amount) public onlyOwner returns (bool) {
