5027047a030d2028ea66da67b4d72c534f763d77
Franklin Richards <powerhousefrank@protonmail.com>
2021-01-21 14:56:30 +0530

    Updated CI for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,11 @@ require("solidity-coverage");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+	const accounts = await ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+	for (const account of accounts) {
+		console.log(account.address);
+	}
 });
 
 // You need to export an object to set up your config
@@ -18,24 +18,23 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: {
-        version: "0.5.17",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200
-            }
-        },
-    },
-    networks: {
-        hardhat: {
-        },
-    },
-    paths: {
-        sources: "./contracts",
-        tests: "./tests-js",
-    },
-    mocha: {
-        timeout: 600000
-    }
+	solidity: {
+		version: "0.5.17",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	networks: {
+		hardhat: {},
+	},
+	paths: {
+		sources: "./contracts",
+		tests: "./tests-js",
+	},
+	mocha: {
+		timeout: 600000,
+	},
 };
