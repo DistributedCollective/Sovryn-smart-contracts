a30b59e4e83a3e7e12e9d1a082de8ef5843983c5
Tyrone Johnson <tjcloa@gmail.com>
2021-01-26 13:26:33 +0300

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,25 @@
+const { task } = require("hardhat/config");
+
 require("@nomiclabs/hardhat-truffle5");
-require("solidity-coverage");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-web3");
+require("hardhat-contract-sizer"); //yarn run hardhat size-contracts
+require("solidity-coverage"); // $ npx hardhat coverage
+require("hardhat-log-remover");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
+/* this is for use with etheres.js 
 task("accounts", "Prints the list of accounts", async () => {
 	const accounts = await ethers.getSigners();
 
 	for (const account of accounts) {
 		console.log(account.address);
 	}
+});
+*/
+task("accounts", "Prints accounts", async (_, { web3 }) => {
+	console.log(await web3.eth.getAccounts());
 });
 
 // You need to export an object to set up your config
@@ -17,6 +28,12 @@ task("accounts", "Prints the list of accounts", async () => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+/*contractSizer: {
+		alphaSort: false,
+		runOnCompile: true,
+		disambiguatePaths: false,
+},*/
+
 module.exports = {
 	solidity: {
 		version: "0.5.17",
