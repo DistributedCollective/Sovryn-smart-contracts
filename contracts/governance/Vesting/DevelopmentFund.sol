ad1c6d91f06e1e81a27ea433a1f2cb5979983dfd
Franklin Richards <powerhousefrank@protonmail.com>
2021-01-31 02:24:58 +0530

    Development Fund Alpha Contract Added

---
+++ contracts/governance/Vesting/DevelopmentFund.sol
@@ -0,0 +1,315 @@
+pragma solidity ^0.5.17;
+
+import "../../openzeppelin/Ownable.sol";
+import "../../openzeppelin/SafeMath.sol";
+import "../../interfaces/IERC20.sol";
+
+/**
+ *  @title A holding contract for Sovryn Development Fund.
+ *  @author Franklin Richards
+ *  @notice You can use this contract for timed token release from Dev Fund.
+ */
+contract DevelopmentFund is Ownable {
+	using SafeMath for uint256;
+
+	/* Storage */
+
+	/// @notice The SOV token contract.
+	IERC20 public SOV;
+
+	/// @notice The owner of the locked tokens.
+	address public lockedTokenOwner;
+	/// @notice The owner of the unlocked tokens.
+	address public unlockedTokenOwner;
+	/// @notice The emergency transfer wallet/contract.
+	address public safeVault;
+	/// @notice The new locked token owner waiting to be approved.
+	address public newLockedTokenOwner;
+
+	/// @notice The last token release timestamp or the time of contract creation.
+	uint256 public lastReleaseTime;
+	/// @notice The amount of remaining tokens to release.
+	uint256 public remainingTokens;
+
+	/// @notice The release duration array in seconds.
+	uint256[] public releaseDuration;
+	/// @notice The release token amount.
+	uint256[] public releaseTokenAmount;
+
+	/* Events */
+
+	/// @notice Emitted when a new locked owner is added to the contract.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _newLockedOwner The address which is added as the new locked owner.
+	/// @dev Can only be initiated by the current locked owner.
+	event NewLockedOwnerAdded(address indexed _initiator, address indexed _newLockedOwner);
+
+	/// @notice Emitted when a new locked owner is approved to the contract.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _oldLockedOwner The address of the previous locked owner.
+	/// @param _newLockedOwner The address which is added as the new locked owner.
+	/// @dev Can only be initiated by the current unlocked owner.
+	event NewLockedOwnerApproved(address indexed _initiator, address indexed _oldLockedOwner, address indexed _newLockedOwner);
+
+	/// @notice Emitted when a new unlocked owner is updated in the contract.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _newUnlockedOwner The address which is updated as the new unlocked owner.
+	/// @dev Can only be initiated by the current locked owner.
+	event UnlockedOwnerUpdated(address indexed _initiator, address indexed _newUnlockedOwner);
+
+	/// @notice Emitted when a new token deposit is done.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _amount The total amount of token deposited.
+	event TokenDeposit(address indexed _initiator, uint256 indexed _amount);
+
+	/// @notice Emitted when a new release schedule is created.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _releaseCount The number of releases planned in the schedule.
+	event TokenReleaseChanged(address indexed _initiator, uint256 indexed _releaseCount);
+
+	/// @notice Emitted when a locked owner withdraws all or part of the token balance from contract.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _receiver The address which receives this token withdrawed.
+	/// @param _amount The total amount of token withdrawed.
+	event LockedTokenWithdrawalByLockedOwner(address indexed _initiator, address indexed _receiver, uint256 indexed _amount);
+
+	/// @notice Emitted when a unlocked owner transfers all the tokens to a safe vault.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _amount The total amount of token transferred.
+	/// @dev This is done in an emergency situation only to a predetermined wallet by governance.
+	event LockedTokenTransferByUnlockedOwner(address indexed _initiator, uint256 indexed _amount);
+
+	/// @notice Emitted when a unlocked owner withdraws the released tokens.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _amount The total amount of token withdrawed.
+	/// @param _releaseCount The total number of releases done based on duration.
+	event UnlockedTokenWithdrawalByUnlockedOwner(address indexed _initiator, uint256 indexed _amount, uint256 indexed _releaseCount);
+
+	/// @notice Emitted when a locked owner transfers all the tokens to a receiver.
+	/// @param _initiator The address which initiated this event to be emitted.
+	/// @param _receiver The address which receives this token transfer.
+	/// @param _amount The total amount of token transferred.
+	/// @dev This is done only by governance.
+	event LockedTokenTransferByLockedOwner(address indexed _initiator, address indexed _receiver, uint256 indexed _amount);
+
+	/* Modifiers */
+
+	modifier onlyUnlockedTokenOwner() {
+		require(msg.sender == unlockedTokenOwner, "Only Unlocked Token Owner can call this.");
+		_;
+	}
+
+	/* Functions */
+
+	/**
+	 * @notice Setup the required parameters.
+	 * @param _SOV The SOV token address.
+	 * @param _lockedTokenOwner The owner of the locked tokens & contract.
+	 * @param _safeVault The emergency wallet/contract to transfer token.
+	 * @param _unlockedTokenOwner The owner of the unlocked tokens.
+	 */
+	constructor(
+		address _SOV,
+		address _lockedTokenOwner,
+		address _safeVault,
+		address _unlockedTokenOwner
+	) public {
+		require(_SOV != address(0), "Invalid SOV Address.");
+		require(_lockedTokenOwner != address(0), "Locked token & contract owner address invalid.");
+		require(_safeVault != address(0), "Safe Vault address invalid.");
+		require(_unlockedTokenOwner != address(0), "Unlocked token address invalid.");
+
+		SOV = IERC20(_SOV);
+		lockedTokenOwner = _lockedTokenOwner;
+		safeVault = _safeVault;
+		unlockedTokenOwner = _unlockedTokenOwner;
+
+		lastReleaseTime = block.timestamp;
+
+		_transferOwnership(_lockedTokenOwner);
+	}
+
+	/**
+	 * @notice Update Locked Token Owner.
+	 * @param _newLockedTokenOwner The owner of the locked tokens & contract.
+	 */
+	function updateLockedTokenOwner(address _newLockedTokenOwner) public onlyOwner {
+		require(_newLockedTokenOwner != address(0), "New locked token owner address invalid.");
+
+		newLockedTokenOwner = _newLockedTokenOwner;
+
+		emit NewLockedOwnerAdded(msg.sender, _newLockedTokenOwner);
+	}
+
+	/**
+	 * @notice Approve Locked Token Owner.
+	 * @dev This approval is an added security to avoid development fund takeover by a compromised governance.
+	 */
+	function approveLockedTokenOwner() public onlyUnlockedTokenOwner {
+		require(newLockedTokenOwner != address(0), "No new locked owner added.");
+
+		emit NewLockedOwnerApproved(msg.sender, lockedTokenOwner, newLockedTokenOwner);
+
+		lockedTokenOwner = newLockedTokenOwner;
+
+		_transferOwnership(lockedTokenOwner);
+
+		newLockedTokenOwner = address(0);
+	}
+
+	/**
+	 * @notice Update Unlocked Token Owner.
+	 * @param _newUnlockedTokenOwner The new unlocked token owner.
+	 */
+	function updateUnlockedTokenOwner(address _newUnlockedTokenOwner) public onlyOwner {
+		require(_newUnlockedTokenOwner != address(0), "New unlocked token owner address invalid.");
+
+		unlockedTokenOwner = _newUnlockedTokenOwner;
+
+		emit UnlockedOwnerUpdated(msg.sender, _newUnlockedTokenOwner);
+	}
+
+	/**
+	 * @notice Deposit tokens to this contract.
+	 * @param _amount the amount of tokens deposited.
+	 * @dev These tokens can be withdrawn/transferred any time by the lockedTokenOwner.
+	 */
+	function depositTokens(uint256 _amount) public {
+		require(_amount > 0, "Amount needs to be bigger than zero.");
+		require(SOV.allowance(msg.sender, address(this)) >= _amount, "Not enough token approved for transfer.");
+
+		bool txStatus = SOV.transferFrom(msg.sender, address(this), _amount);
+		require(txStatus, "Token transfer was not successful.");
+
+		remainingTokens = remainingTokens.add(_amount);
+
+		emit TokenDeposit(msg.sender, _amount);
+	}
+
+	/**
+	 * @notice Change the Token release schedule. It creates a completely new schedule, and does not append on the previous one.
+	 * @param _newLastReleaseTime If the last release time is to be changed, zero if no change required.
+	 * @param _releaseDuration The time duration between each release calculated from `lastReleaseTime` in seconds.
+	 * @param _releaseTokenAmount The amount of token to be released in each duration/interval.
+	 * @dev _releaseDuration and _releaseTokenAmount should be specified in reverse order of release.
+	 */
+	function changeTokenReleaseSchedule(
+		uint256 _newLastReleaseTime,
+		uint256[] memory _releaseDuration,
+		uint256[] memory _releaseTokenAmount
+	) public onlyOwner {
+		/// Checking if the schedule duration and token allocation length matches.
+		require(_releaseDuration.length == _releaseTokenAmount.length, "Release Schedule does not match.");
+
+		/// If the last release time has to be changed, then you can pass a new one here.
+		/// Or else, the duration of release will be calculated based on this timestamp.
+		if (_newLastReleaseTime != 0) {
+			lastReleaseTime = _newLastReleaseTime;
+		}
+
+		/// Checking if the contract have enough token balance for the release.
+		uint256 _releaseTotalTokenAmount;
+		for (uint256 amountIndex = 0; amountIndex < _releaseTokenAmount.length; amountIndex++) {
+			_releaseTotalTokenAmount = _releaseTotalTokenAmount.add(_releaseTokenAmount[amountIndex]);
+		}
+
+		/// If the token balance is not sufficient, then we transfer the change to contract.
+		if (remainingTokens < _releaseTotalTokenAmount) {
+			bool txStatus = SOV.transferFrom(msg.sender, address(this), _releaseTotalTokenAmount.sub(remainingTokens));
+			require(txStatus, "Not enough token sent to change release schedule.");
+		} else if (remainingTokens > _releaseTotalTokenAmount) {
+			/// If there are more tokens than required, send the extra tokens back.
+			bool txStatus = SOV.transferFrom(msg.sender, address(this), remainingTokens.sub(_releaseTotalTokenAmount));
+			require(txStatus, "Not enough token sent to change release schedule.");
+		}
+
+		/// Updating the remaining tokens in contract
+		remainingTokens = _releaseTotalTokenAmount;
+
+		/// Finally we update the token release schedule.
+		releaseDuration = _releaseDuration;
+		releaseTokenAmount = _releaseTokenAmount;
+
+		emit TokenReleaseChanged(msg.sender, _releaseDuration.length);
+	}
+
+	/**
+	 * @notice Withdraws all or part of the remaining tokens.
+	 * @param _amount the amount of tokens to be withdrawn.
+	 * @param _receiver the receiving address.
+	 */
+	function withdrawTokensByLockedTokenOwner(uint256 _amount, address _receiver) public onlyOwner {
+		require(_amount > 0, "Token amount needs to be bigger than 0.");
+		require(_amount <= remainingTokens, "Token amount is not available.");
+		require(_receiver != address(0), "Receiver address is invalid");
+
+		remainingTokens = remainingTokens.sub(_amount);
+
+		bool txStatus = SOV.transfer(_receiver, _amount);
+		require(txStatus, "Token transfer was not successful. Check receiver address.");
+
+		emit LockedTokenWithdrawalByLockedOwner(msg.sender, _receiver, _amount);
+	}
+
+	/**
+	 * @notice Transfers all of the remaining tokens in an emergency situation.
+	 * @dev This could be called when governance or development fund might be compromised.
+	 */
+	function transferTokensByUnlockedTokenOwner() public onlyUnlockedTokenOwner {
+		bool txStatus = SOV.transfer(safeVault, remainingTokens);
+		require(txStatus, "Token transfer was not successful. Check receiver address.");
+
+		emit LockedTokenTransferByUnlockedOwner(msg.sender, remainingTokens);
+
+		remainingTokens = 0;
+	}
+
+	/**
+	 * @notice Withdraws all unlocked/released token.
+	 */
+	function withdrawTokensByUnlockedTokenOwner() public onlyUnlockedTokenOwner {
+		uint256 count; /// To know how many elements to be removed from the release schedule.
+		uint256 amount; /// To know the total amount to be transferred.
+		uint256 totalDuration; /// To know the total duration covered from the last release time.
+		uint256 lastReleaseTimeMemory = lastReleaseTime; /// Better to use memory than storage.
+		uint256 releaseDurationLength = releaseDuration.length.sub(1); /// Also checks if there are any elements in the release schedule.
+
+		/// Getting the amount of tokens, the number of releases and calculating the total duration.
+		while (lastReleaseTimeMemory.add(releaseDuration[releaseDurationLength].add(totalDuration)) >= block.timestamp) {
+			amount = amount.add(releaseTokenAmount[releaseDurationLength]);
+			totalDuration = totalDuration.add(releaseDuration[releaseDurationLength]);
+			releaseDurationLength--;
+			count++;
+		}
+
+		emit UnlockedTokenWithdrawalByUnlockedOwner(msg.sender, amount, count);
+
+		/// Now clearing up the release schedule.
+		while (count > 0) {
+			releaseDuration.length--;
+			releaseTokenAmount.length--;
+			count--;
+		}
+
+		/// Now updating the last release time to reflect the current last release.
+		lastReleaseTime = lastReleaseTimeMemory.add(totalDuration);
+
+		/// Sending the amount to unlocked token owner.
+		bool txStatus = SOV.transfer(unlockedTokenOwner, amount);
+		require(txStatus, "Token transfer was not successful. Check receiver address.");
+	}
+
+	/**
+	 * @notice Transfers all of the remaining tokens by the owner maybe for an upgrade.
+	 * @dev This could be called when the current development fund has to be upgraded.
+	 * @param _receiver The address which receives this token transfer.
+	 */
+	function transferTokensByLockedTokenOwner(address _receiver) public onlyOwner {
+		bool txStatus = SOV.transfer(_receiver, remainingTokens);
+		require(txStatus, "Token transfer was not successful. Check receiver address.");
+
+		emit LockedTokenTransferByLockedOwner(msg.sender, _receiver, remainingTokens);
+
+		remainingTokens = 0;
+	}
+}
