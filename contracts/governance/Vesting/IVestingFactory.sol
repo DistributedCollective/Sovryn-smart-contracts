c6f01b31b4874b18da762b46bf1eaa7ab6c909ea
James Howlett <jameshowlett977@gmail.com>
2021-02-05 11:22:35 +0200

    fixed IVestingFactory

--- contracts/governance/Vesting/IVestingFactory.sol
+++ contracts/governance/Vesting/IVestingFactory.sol
@@ -20,13 +20,4 @@ interface IVestingFactory {
 		address _feeSharing,
 		address _owner
 	) external returns (address);
-
-	function deployDevelopmentVesting(
-		address _SOV,
-		address _tokenOwner,
-		uint256 _cliff,
-		uint256 _duration,
-		uint256 _frequency,
-		address _owner
-	) external returns (address);
 }
