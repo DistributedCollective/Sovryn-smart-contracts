687bde5684a4295f0d9ef350d93d796b2bbe944f
James Howlett <jameshowlett977@gmail.com>
2021-02-10 10:06:53 +0200

    code formatting

--- contracts/proxy/UpgradableProxy.sol
+++ contracts/proxy/UpgradableProxy.sol
@@ -6,21 +6,19 @@ import "./Proxy.sol";
  * @title Upgradable Proxy Contract
  */
 contract UpgradableProxy is Proxy {
+	/**
+	 * @notice Sets address of the implementation
+	 * @param _implementation Address of the implementation
+	 */
+	function setImplementation(address _implementation) public onlyProxyOwner {
+		_setImplementation(_implementation);
+	}
 
-    /**
-     * @notice Sets address of the implementation
-     * @param _implementation Address of the implementation
-     */
-    function setImplementation(address _implementation) public onlyProxyOwner {
-        _setImplementation(_implementation);
-    }
-
-    /**
+	/**
 	 * @notice Sets address of the owner
 	 * @param _owner Address of the owner
 	 */
-    function setProxyOwner(address _owner) public onlyProxyOwner {
-        _setProxyOwner(_owner);
-    }
-
+	function setProxyOwner(address _owner) public onlyProxyOwner {
+		_setProxyOwner(_owner);
+	}
 }
