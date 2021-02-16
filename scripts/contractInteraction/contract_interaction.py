95eb79b59b29cd8ab30704bc4b10eb4ab9e1e747
James Howlett <jameshowlett977@gmail.com>
2021-02-16 15:29:41 +0200

    updated createProposalSIP007

--- scripts/contractInteraction/contract_interaction.py
+++ scripts/contractInteraction/contract_interaction.py
@@ -730,7 +730,7 @@ def createProposalSIP007():
     vestingRegistry = Contract.from_abi("CrowdSaleMethods", address=contracts['VestingRegistry'], abi=CrowdSaleMethods.abi, owner=acct)
 
     # amount = 7776308611443494927
-    amount = 10*16
+    amount = 10**16
 
     # action 1
     target = contracts['governorVault']
