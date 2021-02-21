8ae9e0310c77cddffecd984027ec936134456c64
Tyrone Johnson <tjcloa@gmail.com>
2021-02-21 04:22:51 +0300

    refactoring after audit

--- contracts/governance/Vesting/OriginInvestorsClaim.sol
+++ contracts/governance/Vesting/OriginInvestorsClaim.sol
@@ -7,6 +7,7 @@ import "../Staking/Staking.sol";
 /**
  * @title Origin investors claim vested cSOV tokens
  * @notice // TODO: fund this contract with a total amount of SOV needed to distribute
+ *
  */
 contract OriginInvestorsClaim is Ownable {
 	using SafeMath for uint256;
@@ -14,50 +15,37 @@ contract OriginInvestorsClaim is Ownable {
 	//VestingRegistry public constant vestingRegistry = VestingRegistry(0x80B036ae59B3e38B573837c01BB1DB95515b7E6B);
 
 	///@notice constant used for computing the vesting dates
-	uint256 public constant SIX_WEEKS = 6 weeks;
-
-	uint256 public constant SOV_VESTING_CLIFF = SIX_WEEKS;
-	uint256 public constant SOV_VESTING_DURATION = SOV_VESTING_CLIFF;
+	uint256 public constant SOV_VESTING_CLIFF = 6 weeks;
 
 	VestingRegistry public vestingRegistry;
 	Staking public staking;
 	uint256 public kickoffTS;
 	IERC20 public SOVToken;
 	uint256 public vestingTerm;
+	bool public investorsListInitialized;
 
-	//user => flag whether user has already claimed vested SOV
-	mapping(address => bool) public vestingList;
-	//user => flag whether user has already withdrawn SOV
-	mapping(address => bool) public withdrawnList;
 	//user => flag whether user has admin role
 	mapping(address => bool) public admins;
 
-	mapping(address => uint256) public investorsAmountsList; // origin investors entitled to claim SOV
+	// origin investors entitled to claim SOV
+	mapping(address => uint256) public investorsAmountsList;
 
 	event AdminAdded(address admin);
 	event AdminRemoved(address admin);
-	event InvestorsAmountsListSet(uint256 qty);
+	event InvestorsAmountsListSet(uint256 qty, uint256 totalAmount);
+	event ClaimVested(address indexed investor, uint256 amount);
+	event ClaimTransferred(address indexed investor, uint256 amount);
 
 	/**
 	 * @dev Throws if called by any account other than the owner or admin.
 	 */
 	modifier onlyAuthorized() {
-		require(isOwner() || admins[msg.sender], "unauthorized");
+		require(isOwner() || admins[msg.sender], "OriginInvestorsClaim::onlyAuthorized: unauthorized");
 		_;
 	}
 
 	modifier onlyWhitelisted() {
-		require(investorsAmountsList[msg.sender] != 0, "not whitelisted");
-		_;
-	}
-
-	modifier notInVestingList() {
-		require(!vestingList[msg.sender], "address is in the vestingList");
-		_;
-	}
-
-	modifier notWithdrawn() {
-		require(!withdrawnList[msg.sender], "tokens already withdrawn");
+		require(investorsAmountsList[msg.sender] != 0, "OriginInvestorsClaim::onlyWhitelisted: not whitelisted or already claimed");
 		_;
 	}
 
@@ -80,46 +68,83 @@ contract OriginInvestorsClaim is Ownable {
 	}
 
 	//in case we have unclaimed tokens
-	function ownerTtransferBalance(address toAddress) public onlyOwner {
-		SOVToken.transfer(toAddress, SOVToken.balanceOf(address(this)));
+	function authorizedBalanceWithdraw(address toAddress) public onlyAuthorized {
+		require(
+			SOVToken.transfer(toAddress, SOVToken.balanceOf(address(this))),
+			"OriginInvestorsClaim::authorizedTransferBalance: transfer failed"
+		);
 	}
 
-	function setInvestorsAmountsList(address[] memory investors, uint256[] memory claimAmounts) public onlyAuthorized {
-		require(investors.length == claimAmounts.length, "investors.length != claimAmounts.length");
+	/**
+	 *  @notice the contract should be approved or transferred necessary amount of SOV prior to calling the function
+	 *	@param allowanceFrom address that approved total amount for this contract or 0x0 if transferred
+	 *  @param investors is the list of investors addresses
+	 *  @param claimAmounts is the list of amounts for investors investors[i] will receive claimAmounts[i] of SOV
+	 */
+	function setInvestorsAmountsList(
+		address[] memory investors,
+		uint256[] memory claimAmounts,
+		address allowanceFrom
+	) public onlyAuthorized {
+		uint256 totalAmount;
+		require(
+			investors.length == claimAmounts.length,
+			"OriginInvestorsClaim::setInvestorsAmountsList: investors.length != claimAmounts.length"
+		);
+		require(!investorsListInitialized, "OriginInvestorsClaim::setInvestorsAmountsList: the investors list has already been set");
+		investorsListInitialized = true;
+
 		for (uint256 i = 0; i < investors.length; i++) {
 			investorsAmountsList[investors[i]] = claimAmounts[i];
+			totalAmount = totalAmount.add(claimAmounts[i]);
 		}
 
-		emit InvestorsAmountsListSet(investors.length);
+		require(
+			SOVToken.balanceOf(address(this)) >= totalAmount ||
+				(allowanceFrom != address(0) && SOVToken.transferFrom(allowanceFrom, address(this), totalAmount)),
+			"OriginInvestorsClaim::setInvestorsAmountsList: the contract is not enough financed or wrong allowance address passed"
+		);
+
+		emit InvestorsAmountsListSet(investors.length, totalAmount);
 	}
 
 	function claim() public onlyWhitelisted {
 		if (now < vestingTerm) {
 			createVesting();
 		} else {
-			withdraw();
+			transfer();
 		}
 	}
 
-	function createVesting() internal notInVestingList {
+	function createVesting() internal {
 		uint256 cliff = vestingTerm.sub(now);
 		uint256 duration = cliff;
 		uint256 amount = investorsAmountsList[msg.sender];
 		address vestingContractAddress;
 
-		vestingList[msg.sender] = true;
+		vestingContractAddress = vestingRegistry.getVesting(msg.sender);
+		require(vestingContractAddress == address(0), "OriginInvestorsClaim::withdraw: investor has an active vesting contract");
+
+		delete investorsAmountsList[msg.sender];
 
 		vestingRegistry.createVesting(msg.sender, amount, cliff, duration);
 		vestingContractAddress = vestingRegistry.getVesting(msg.sender);
+		require(SOVToken.transfer(address(vestingRegistry), amount), "OriginInvestorsClaim::withdraw: SOV transfer failed");
 		vestingRegistry.stakeTokens(vestingContractAddress, amount);
+
+		emit ClaimVested(msg.sender, amount);
 	}
 
-	function withdraw() internal notInVestingList notWithdrawn {
-		withdrawnList[msg.sender] = true;
+	function transfer() internal {
+		uint256 amount = investorsAmountsList[msg.sender];
+
+		delete investorsAmountsList[msg.sender];
 
 		// withdraw only for those claiming after the cliff, i.e. without vesting contracts
 		// those with vestingContracts should withdraw using Vesting.withdrawTokens
 		// from Vesting (VestingLogic) contract
-		SOVToken.transfer(msg.sender, investorsAmountsList[msg.sender]);
+		require(SOVToken.transfer(msg.sender, amount), "OriginInvestorsClaim::withdraw: SOV transfer failed");
+
+		emit ClaimTransferred(msg.sender, amount);
 	}
 }
--- tests-js/OriginInvestorsClaim.test.js
+++ tests-js/OriginInvestorsClaim.test.js
@@ -2,15 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { expectRevert, expectEvent, constants, BN, balance, time } = require("@openzeppelin/test-helpers");
 
-const {
-	encodeParameters,
-	etherMantissa,
-	mineBlock,
-	increaseTime,
-	blockNumber,
-	setNextBlockTimestamp,
-	lastBlock,
-} = require("./Utils/Ethereum");
+const { mineBlock, setNextBlockTimestamp } = require("./Utils/Ethereum");
 
 const StakingLogic = artifacts.require("Staking");
 const StakingProxy = artifacts.require("StakingProxy");
@@ -23,11 +15,7 @@ const VestingRegistry = artifacts.require("VestingRegistry");
 const OriginInvestorsClaim = artifacts.require("OriginInvestorsClaim");
 
 const ONE_WEEK = new BN(7 * 24 * 60 * 60);
-const FOUR_WEEKS = ONE_WEEK.muln(4); //new BN(4 * 7 * 24 * 60 * 60);
 const SIX_WEEKS = ONE_WEEK.muln(6); //new BN(6 * 7 * 24 * 60 * 60);
-
-const VESTING_CLIFF = SIX_WEEKS;
-const DURATION = VESTING_CLIFF;
 
 const TOTAL_SUPPLY = "100000000000000000000000000";
 const ONE_MILLION = "1000000000000000000000000";
@@ -36,12 +24,13 @@ const ZERO_ADDRESS = constants.ZERO_ADDRESS;
 const pricsSats = "2500";
 
 contract("OriginInvestorsClaim", (accounts) => {
-	let root, account1, account2, account3, investor1, investor2, investor3;
+	let root, initializer, account1, investor1, investor2, investor3;
 	let SOV, kickoffTS;
 	let staking, stakingLogic, feeSharingProxy;
-	let vestingFactory, vestingLogic, vestingRegistry, investorsClaim;
+	let vestingFactory, vestingLogic, vestingRegistry;
 	let investors;
 	let amounts, amount1, amount2, amount3;
+	let investorsClaim;
 
 	function getTimeFromKickoff(offset) {
 		return kickoffTS.add(new BN(offset));
@@ -58,8 +47,41 @@ contract("OriginInvestorsClaim", (accounts) => {
 		});
 	}
 
+	async function createOriginInvestorsClaimContract({
+		initializeInvestorsList = false,
+		approve = true,
+		transfer = false,
+		_investors = investors,
+		_amounts = amounts,
+	}) {
+		investorsClaim = await OriginInvestorsClaim.new(vestingRegistry.address);
+		if (initializeInvestorsList) {
+			await fundContract(approve, transfer, _amounts);
+			if (approve) {
+				investorsClaim.setInvestorsAmountsList(_investors, _amounts, initializer);
+			} else if (transfer) {
+				investorsClaim.setInvestorsAmountsList(_investors, _amounts, constants.ZERO_ADDRESS);
+			}
+		}
+	}
+
+	async function fundContract({ approve = true, transfer = false, _amounts = amounts }) {
+		const totalReducer = (accumulator, currentValue) => accumulator.add(currentValue);
+		const total = _amounts.reduce(totalReducer);
+		await SOV.transfer(initializer, total);
+		await investorsClaim.authorizedBalanceWithdraw(root); //nullify balance
+		if (!approve && !transfer) throw "createOriginInvestorsClaimContract: the contract is not funded";
+
+		if (approve) {
+			await SOV.approve(investorsClaim.address, total, { from: initializer });
+		} else {
+			// transfer
+			await SOV.transfer(investorsClaim.address, total, { from: initializer });
+		}
+	}
+
 	before(async () => {
-		[root, account1, account2, account3, investor1, investor2, investor3, ...accounts] = accounts;
+		[root, initializer, account1, investor1, investor2, investor3, ...accounts] = accounts;
 		investors = [investor1, investor2, investor3];
 		amount1 = new BN(ONE_MILLION);
 		amount2 = amount1.muln(2);
@@ -94,57 +116,115 @@ contract("OriginInvestorsClaim", (accounts) => {
 
 		kickoffTS = await staking.kickoffTS.call();
 
-		investorsClaim = await OriginInvestorsClaim.new(vestingRegistry.address);
+		await createOriginInvestorsClaimContract({ initializeInvestorsList: true, approve: true, transfer: false });
 
-		await SOV.transfer(investorsClaim.address, amount1.add(amount2).add(amount3));
 		await vestingRegistry.addAdmin(investorsClaim.address); //TODO: after deploy run addAdmin(OriginInvestorsClaim address)
-		await investorsClaim.setInvestorsAmountsList(investors, amounts);
 	});
 
 	describe("setInvestorsList", () => {
+		beforeEach(async () => {
+			await createOriginInvestorsClaimContract({ initializeInvestorsList: false });
+			await fundContract({ approve: true, transfer: false });
+		});
+
 		it("set investors list", async () => {
-			let tx = await investorsClaim.setInvestorsAmountsList(investors, amounts);
-			expectEvent(tx, "InvestorsAmountsListSet", { qty: new BN(amounts.length) });
-			for (i = 0; i < investors; i++) {
-				amount = await investorsClaim.investorsAmountsList(invertors[i]);
-				expect(amount).to.be.bignumber.equal(new BN(amounts[i]), `wrong investors list assignment at index ${i}`);
+			// transfer
+
+			await fundContract({ approve: false, transfer: true });
+
+			let tx = await investorsClaim.setInvestorsAmountsList(investors, amounts, constants.ZERO_ADDRESS);
+			expectEvent(tx, "InvestorsAmountsListSet", { qty: new BN(amounts.length), totalAmount: amount1.add(amount2).add(amount3) });
+
+			await verifyAmounts();
+
+			// allowance
+			await createOriginInvestorsClaimContract({ initializeInvestorsList: false });
+			await fundContract({ approve: true, transfer: false });
+
+			tx = await investorsClaim.setInvestorsAmountsList(investors, amounts, initializer);
+			expectEvent(tx, "InvestorsAmountsListSet", { qty: new BN(amounts.length), totalAmount: amount1.add(amount2).add(amount3) });
+
+			await verifyAmounts();
+
+			async function verifyAmounts() {
+				for (i = 0; i < investors; i++) {
+					amount = await investorsClaim.investorsAmountsList(invertors[i]);
+					expect(amount).to.be.bignumber.equal(new BN(amounts[i]), `wrong investors list assignment at index ${i}`);
+				}
 			}
+
+			//allowance with wrong 0x0 address param
+			await createOriginInvestorsClaimContract({ initializeInvestorsList: false });
+			await fundContract({ approve: true, transfer: false });
+			//await SOV.transfer(root, await SOV.balanceOf(initializer), { from: initializer });
+
+			await expectRevert(
+				investorsClaim.setInvestorsAmountsList(investors, amounts, constants.ZERO_ADDRESS),
+				"OriginInvestorsClaim::setInvestorsAmountsList: the contract is not enough financed or wrong allowance address passed"
+			);
+		});
+
+		it("can load investors list only once", async () => {
+			await createOriginInvestorsClaimContract({ initializeInvestorsList: true, transfer: true });
+			//await investorsClaim.setInvestorsAmountsList(investors, amounts, initializer); // if transferred then allowanceFrom (initializer) doesn't matter
+			await expectRevert(
+				investorsClaim.setInvestorsAmountsList(investors, amounts, constants.ZERO_ADDRESS),
+				"OriginInvestorsClaim::setInvestorsAmountsList: the investors list has already been set"
+			);
+			await expectRevert(
+				investorsClaim.setInvestorsAmountsList(investors, amounts, initializer),
+				"OriginInvestorsClaim::setInvestorsAmountsList: the investors list has already been set"
+			);
 		});
 
 		it("fails if investors.length != amounts.length", async () => {
 			const investorsReduced = investors.slice(1);
 			await expectRevert(
-				investorsClaim.setInvestorsAmountsList(investorsReduced, amounts),
+				investorsClaim.setInvestorsAmountsList(investorsReduced, amounts, initializer),
 				"investors.length != claimAmounts.length"
 			);
 		});
 
 		it("only authorised can whitelist investors", async () => {
-			await expectRevert(investorsClaim.setInvestorsAmountsList(investors, amounts, { from: account1 }), "unauthorized");
+			await expectRevert(investorsClaim.setInvestorsAmountsList(investors, amounts, initializer, { from: account1 }), "unauthorized");
 		});
 	});
 
-	describe("process claims", () => {
+	describe.only("process claims", () => {
+		before(async () => {
+			await createOriginInvestorsClaimContract({ initializeInvestorsList: true });
+			await vestingRegistry.addAdmin(investorsClaim.address);
+			//await fundContract({ approve: true, transfer: false });
+		});
 		it("should create vesting contract within vesting period", async () => {
 			const timeFromKickoff = getTimeFromKickoff(ONE_WEEK);
 			await setNextBlockTimestamp(timeFromKickoff.toNumber());
-			await SOV.transfer(vestingRegistry.address, amount1);
 			tx = await investorsClaim.claim({ from: investor1 });
+
+			await expectEvent(tx.receipt, "ClaimVested", {
+				investor: investor1,
+				amount: amount1,
+			});
+
 			let txHash = tx.receipt.transactionHash;
 
-			checkVestingContractCreated(txHash, investor1, SIX_WEEKS.sub(ONE_WEEK).subn(1), amount1);
+			checkVestingContractCreated(txHash, investor1, SIX_WEEKS.sub(ONE_WEEK), amount1);
 
-			expect(await investorsClaim.vestingList(investor1)).to.be.true;
+			expect(await investorsClaim.investorsAmountsList(investor1)).to.be.bignumber.equal(new BN(0));
 
-			await SOV.transfer(vestingRegistry.address, amount2);
 			await setNextBlockTimestamp(getTimeFromKickoff(SIX_WEEKS).subn(1).toNumber());
 			tx = await investorsClaim.claim({ from: investor2 });
-			//console.log(tx.receipt);
+
+			await expectEvent(tx.receipt, "ClaimVested", {
+				investor: investor2,
+				amount: amount2,
+			});
+
 			txHash = tx.receipt.transactionHash;
 
 			checkVestingContractCreated(txHash, investor2, new BN(1), amount2);
 
-			expect(await investorsClaim.vestingList(investor2)).to.be.true;
+			expect(await investorsClaim.investorsAmountsList(investor2)).to.be.bignumber.equal(new BN(0));
 
 			//TODO: check vesting created and the user is in the list
 		});
@@ -155,37 +235,49 @@ contract("OriginInvestorsClaim", (accounts) => {
 			let balance;
 			// investor2 claims within cliff - vesting contract created with amounts[1]
 			// move time 1 ms past cliff
-			await setNextBlockTimestamp((await getTimeFromKickoff(SIX_WEEKS)).addn(1).toNumber());
+			await setNextBlockTimestamp((await getTimeFromKickoff(SIX_WEEKS)).addn(100).toNumber());
 			await mineBlock();
 
 			balance = await SOV.balanceOf(investor3);
 			expect(balance).to.be.bignumber.equal(new BN(0));
 
-			await SOV.transfer(vestingRegistry.address, amount3);
-			await investorsClaim.claim({ from: investor3 });
+			// await SOV.transfer(vestingRegistry.address, amount3);
+			tx = await investorsClaim.claim({ from: investor3 });
+
+			await expectEvent(tx.receipt, "ClaimTransferred", {
+				investor: investor3,
+				amount: amount3,
+			});
+
 			balance = await SOV.balanceOf(investor3);
-			expect(balance).to.be.bignumber.equal(amounts[2]);
+			expect(balance).to.be.bignumber.equal(amount3);
 		});
 
 		it("investors with vesting contracts created cannot withdraw here", async () => {
-			await expectRevert(investorsClaim.claim({ from: investor1 }), "address is in the vestingList");
+			await expectRevert(
+				investorsClaim.claim({ from: investor1 }),
+				"OriginInvestorsClaim::onlyWhitelisted: not whitelisted or already claimed"
+			);
 		});
 
 		it("can withdraw only once", async () => {
-			await expectRevert(investorsClaim.claim({ from: investor3 }), "tokens already withdrawn");
+			await expectRevert(
+				investorsClaim.claim({ from: investor3 }),
+				"OriginInvestorsClaim::onlyWhitelisted: not whitelisted or already claimed"
+			);
 		});
 
 		it("owner should be able to move SOV balances from the contract", async () => {
 			const balanceBefore = await SOV.balanceOf(investorsClaim.address);
 
-			await investorsClaim.ownerTtransferBalance(account1);
+			await investorsClaim.authorizedBalanceWithdraw(account1);
 
 			expect(await SOV.balanceOf(account1)).to.be.bignumber.equal(balanceBefore);
 			expect(await SOV.balanceOf(investorsClaim.address)).to.be.bignumber.equal(new BN(0));
 		});
 
 		it("allows to claim only from whitelisted addresses", async () => {
-			await expectRevert(investorsClaim.claim({ from: account1 }), "not whitelisted");
+			await expectRevert(investorsClaim.claim({ from: account1 }), "not whitelisted or already claimed");
 		});
 	});
 });
