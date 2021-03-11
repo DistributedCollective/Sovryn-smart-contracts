3e3bd2f7e55526458a7a7f1c1c26c2ad9e0e87a0
James Howlett <jameshowlett977@gmail.com>
2021-03-11 09:21:56 +0200

    added OrigingVestingCreator

--- contracts/governance/Vesting/OrigingVestingCreator.sol
+++ contracts/governance/Vesting/OrigingVestingCreator.sol
@@ -5,28 +5,31 @@ import "../../openzeppelin/Ownable.sol";
 import "./VestingRegistry.sol";
 
 /**
-* @title Temp contract for checking address, creating and staking tokens
-*
-*/
+ * @title Temp contract for checking address, creating and staking tokens
+ *
+ */
 contract OrigingVestingCreator is Ownable {
+	VestingRegistry public vestingRegistry;
 
-    VestingRegistry public vestingRegistry;
+	mapping(address => bool) processedList;
 
-    mapping(address => bool) processedList;
+	constructor(address _vestingRegistry) public {
+		vestingRegistry = VestingRegistry(_vestingRegistry);
+	}
 
-    constructor(address _vestingRegistry) public {
-        vestingRegistry = VestingRegistry(_vestingRegistry);
-    }
+	function createVesting(
+		address _tokenOwner,
+		uint256 _amount,
+		uint256 _cliff,
+		uint256 _duration
+	) public onlyOwner {
+		require(_tokenOwner != address(0), "Invalid address");
+		require(!processedList[_tokenOwner], "Already processed");
 
-    function createVesting(address _tokenOwner, uint256 _amount, uint256 _cliff, uint256 _duration) public onlyOwner {
-        require(_tokenOwner != address(0), "Invalid address");
-        require(!processedList[_tokenOwner], "Already processed");
+		processedList[_tokenOwner] = true;
 
-        processedList[_tokenOwner] = true;
-
-        vestingRegistry.createVesting(_tokenOwner, _amount, _cliff, _duration);
-        address vesting = vestingRegistry.getVesting(_tokenOwner);
-        vestingRegistry.stakeTokens(vesting, _amount);
-    }
-
+		vestingRegistry.createVesting(_tokenOwner, _amount, _cliff, _duration);
+		address vesting = vestingRegistry.getVesting(_tokenOwner);
+		vestingRegistry.stakeTokens(vesting, _amount);
+	}
 }
