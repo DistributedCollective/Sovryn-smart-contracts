f7da1f59eb0e97611cd76cf682b8bef18b2cb5e3
Blockchain Moondev <66269471+mooncrypto@users.noreply.github.com>
2021-03-23 09:15:31 -0700

    Update tests-js/other/LoanOpeningsBorrowOrTradeFromPool.test.js
    Co-authored-by: Tyrone Johnson <77393391+tjcloa@users.noreply.github.com>

--- tests-js/other/LoanOpeningsBorrowOrTradeFromPool.test.js
+++ tests-js/other/LoanOpeningsBorrowOrTradeFromPool.test.js
@@ -141,7 +141,7 @@ contract("LoanOpeningsBorrowOrTradeFromPool", (accounts) => {
 				.add(collateralTokenSent);
 
 			// ignore differences in least significant digits due to rounding error
-			expect(Math.abs(expectedPositionSize.sub(new BN(tradeEvent["positionSize"])).toNumber()) < 100).to.be.true;
+			expect(expectedPositionSize.divn(100)).to.be.bignumber.equal(new BN(tradeEvent["positionSize"]).divn(100));
 		});
 
 		it("Test borrowFromPool sim", async () => {
