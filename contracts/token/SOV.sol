f4d5580c7e792b2bf726b4ada21626f0ce19a090
Juan Ignacio Pérez Sacristán <computerphysicslab@gmail.com>
2021-04-08 17:21:57 +0200

    SOV token contact explained

--- contracts/token/SOV.sol
+++ contracts/token/SOV.sol
@@ -6,13 +6,22 @@ import "../openzeppelin/Ownable.sol";
 import "./IApproveAndCall.sol";
 
 /**
- * Sovryn Token
+ * @title Sovryn Token: an ERC-20 token contract for Sovryn governance called SOV
+ * @notice This contract accounts for all holders' balances.
+ * @dev This contract represents a token with dynamic supply.
+ * The owner of the token contract can mint/burn tokens to/from any account
+ * based upon previous governance voting and approval.
  */
 contract SOV is ERC20, ERC20Detailed, Ownable {
 	string constant NAME = "Sovryn Token";
 	string constant SYMBOL = "SOV";
 	uint8 constant DECIMALS = 18;
 
+	/**
+	 * @notice constructor called on deployment, initiates the contract
+	 * @dev on deployment, some amount of tokens will be minted for the owner
+	 * @param _initialAmount the amount of tokens to be minted on contract creation
+	 */
 	constructor(uint256 _initialAmount) public ERC20Detailed(NAME, SYMBOL, DECIMALS) {
 		if (_initialAmount != 0) {
 			_mint(msg.sender, _initialAmount);
@@ -20,7 +29,10 @@ contract SOV is ERC20, ERC20Detailed, Ownable {
 	}
 
 	/**
+	 * @notice creates new tokens and sends them to the recipient
 	 * @dev don't create more than 2^96/10 tokens before updating the governance first
+	 * @param _account the recipient address to get the minted tokens
+	 * @param _amount the amount of tokens to be minted
 	 */
 	function mint(address _account, uint256 _amount) public onlyOwner {
 		_mint(_account, _amount);
@@ -28,6 +40,12 @@ contract SOV is ERC20, ERC20Detailed, Ownable {
 
 	/**
 	 * @notice approves and then calls the receiving contract
+	 * @notice useful to encapsulate sending tokens to a contract in one call
+	 * @notice solidity has no native way to send tokens to contracts
+	 * @notice ERC-20 tokens require approval to be spent by third parties, such as a contract in this case
+	 * @param _spender the contract address to spend the tokens
+	 * @param _amount the amount of tokens to be sent
+	 * @param _data parameters for the contract call, such as endpoint signature
 	 */
 	function approveAndCall(
 		address _spender,
