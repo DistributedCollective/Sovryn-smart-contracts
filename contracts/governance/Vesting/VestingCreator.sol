a31fb9ba9f0883170855ac9be35f468408dcf267
James Howlett <jameshowlett977@gmail.com>
2021-04-20 10:43:12 +0300

    implemented addVestings, process

--- contracts/governance/Vesting/VestingCreator.sol
+++ contracts/governance/Vesting/VestingCreator.sol
@@ -1,17 +1,21 @@
 pragma solidity ^0.5.17;
+pragma experimental ABIEncoderV2;
 
 import "../../interfaces/IERC20.sol";
 import "../../mixins/AdminRole.sol";
 import "./VestingRegistry.sol";
 import "./VestingLogic.sol";
 
-
 contract VestingCreator is AdminRole {
+	using SafeMath for uint;
+
 	///@notice the SOV token contract
-	address public SOV;
+	IERC20 public SOV;
 	VestingRegistry public vestingRegistry;
 
 	VestingData[] public vestingDataList;
+
+	VestingData[] public vestingDataErrorList;
 
 	//TODO check storage slots
 	struct VestingData {
@@ -22,14 +26,15 @@ contract VestingCreator is AdminRole {
 		bool governanceControl;
 	}
 
+	//TODO add events
 
 	event SOVTransferred(address indexed receiver, uint256 amount);
 
 	constructor(address _SOV, address _vestingRegistry) public {
 		require(_SOV != address(0), "SOV address invalid");
-		require(_vestingRegistry != address(0), "invalid address");
+		require(_vestingRegistry != address(0), "Vesting registry address invalid");
 
-		SOV = _SOV;
+		SOV = IERC20(_SOV);
 		vestingRegistry = VestingRegistry(_vestingRegistry);
 	}
 
@@ -42,7 +47,7 @@ contract VestingCreator is AdminRole {
 		require(_receiver != address(0), "receiver address invalid");
 		require(_amount != 0, "amount invalid");
 
-		require(IERC20(SOV).transfer(_receiver, _amount), "transfer failed");
+		require(SOV.transfer(_receiver, _amount), "transfer failed");
 		emit SOVTransferred(_receiver, _amount);
 	}
 
@@ -61,19 +66,20 @@ contract VestingCreator is AdminRole {
 
 		for (uint i = 0; i < _tokenOwners.length; i++) {
 			address vestingAddress = _getVesting(_tokenOwners[i], _governanceControls[i]);
+			VestingData memory vestingData =
+				VestingData({
+					tokenOwner: _tokenOwners[i],
+					amount: _amounts[i],
+					cliff: _cliffs[i],
+					duration: _durations[i],
+					governanceControl: _governanceControls[i]
+				});
 			if (_validateVestingSchedule(vestingAddress, _cliffs[i], _durations[i])) {
-				VestingData memory vestingData =
-					VestingData({
-						tokenOwner: _tokenOwners[i],
-						amount: _amounts[i],
-						cliff: _cliffs[i],
-						duration: _durations[i],
-						governanceControl: _governanceControls[i]
-					});
 				vestingDataList.push(vestingData);
 			} else {
-				//TODO save list with wrong vesting schedule ?
-
+				//account already has vesting contract with different schedule
+				//TODO do we need to save vestings with errors ?
+				vestingDataErrorList.push(vestingData);
 			}
 		}
 	}
@@ -82,9 +88,39 @@ contract VestingCreator is AdminRole {
 		return vestingDataList.length;
 	}
 
-	function process() public {
-		//TODO create 1 or N vestings
+	function getUnprocessedAmount() public view returns (uint) {
+		uint amount = 0;
+		for (uint i = 0; i < vestingDataList.length; i++) {
+			amount = amount.add(vestingDataList[i].amount);
+		}
+		return amount;
+	}
 
+	function process() onlyAuthorized public {
+		if (vestingDataList.length > 0) {
+			VestingData storage vestingData = vestingDataList[vestingDataList.length - 1];
+			uint amount = vestingData.amount;
+			require(SOV.balanceOf(address(this)) >= amount, "balance isn't enough");
+
+			VestingLogic vesting = _createAndGetVesting(vestingData);
+			SOV.approve(address(vesting), amount);
+			vesting.stakeTokens(amount);
+
+			delete vestingDataList[vestingDataList.length - 1];
+		}
+	}
+
+	function clearVestingDataErrorList() onlyAuthorized public {
+		delete vestingDataErrorList;
+	}
+
+	function _createAndGetVesting(VestingData storage vestingData) internal returns (VestingLogic) {
+		if (vestingData.governanceControl) {
+			vestingRegistry.createTeamVesting(vestingData.tokenOwner, vestingData.amount, vestingData.cliff, vestingData.duration);
+		} else {
+			vestingRegistry.createVesting(vestingData.tokenOwner, vestingData.amount, vestingData.cliff, vestingData.duration);
+		}
+		return VestingLogic(_getVesting(vestingData.tokenOwner, vestingData.governanceControl));
 	}
 
 	function _getVesting(address _tokenOwner, bool _governanceControl) internal view returns (address vestingAddress) {
@@ -103,20 +139,4 @@ contract VestingCreator is AdminRole {
 		return (_cliff == vesting.cliff() && _duration == vesting.duration());
 	}
 
-
-//	function createVesting(
-//		address _tokenOwner,
-//		uint256 _amount,
-//		uint256 _cliff,
-//		uint256 _duration
-//	) public onlyOwner {
-//		require(_tokenOwner != address(0), "Invalid address");
-//		require(!processedList[_tokenOwner], "Already processed");
-//
-//		processedList[_tokenOwner] = true;
-//
-//		vestingRegistry.createVesting(_tokenOwner, _amount, _cliff, _duration);
-//		address vesting = vestingRegistry.getVesting(_tokenOwner);
-//		vestingRegistry.stakeTokens(vesting, _amount);
-//	}
 }
