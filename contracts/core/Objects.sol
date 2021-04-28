3e748471fcf55ae057eb9b0f71af7e9e4c6320a9
Juan Ignacio Pérez Sacristán <computerphysicslab@gmail.com>
2021-04-28 09:19:01 +0200

    Protocol Core documented

--- contracts/core/Objects.sol
+++ contracts/core/Objects.sol
@@ -11,4 +11,12 @@ import "./objects/OrderStruct.sol";
 import "./objects/LenderInterestStruct.sol";
 import "./objects/LoanInterestStruct.sol";
 
+/**
+ * @title Objects contract.
+ * @notice This contract code comes from bZx. bZx is a protocol for tokenized
+ * margin trading and lending https://bzx.network similar to the dYdX protocol.
+ *
+ * This contract inherints and aggregates several structures needed to handle
+ * loans on the protocol.
+ * */
 contract Objects is LoanStruct, LoanParamsStruct, OrderStruct, LenderInterestStruct, LoanInterestStruct {}
--- contracts/core/objects/LoanParamsStruct.sol
+++ contracts/core/objects/LoanParamsStruct.sol
@@ -16,26 +16,19 @@ contract LoanParamsStruct {
 	struct LoanParams {
 		/// @dev ID of loan params object.
 		bytes32 id;
-
 		/// @dev If false, this object has been disabled by the owner and can't
 		///   be used for future loans.
 		bool active;
-
 		/// @dev Owner of this object.
 		address owner;
-
 		/// @dev The token being loaned.
 		address loanToken;
-
 		/// @dev The required collateral token.
 		address collateralToken;
-
 		/// @dev The minimum allowed initial margin.
 		uint256 minInitialMargin;
-
 		/// @dev An unhealthy loan when current margin is at or below this value.
 		uint256 maintenanceMargin;
-		
 		/// @dev The maximum term for new loans (0 means there's no max term).
 		uint256 maxLoanTerm;
 	}
