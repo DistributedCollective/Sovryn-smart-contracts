b4105dbe8eb4faccd5b1db8dd08692bdb1159ad6
Franklin Richards <powerhousefrank@protonmail.com>
2021-05-28 19:06:51 +0530

    Deployment script of Multisig

---
+++ scripts/uniswap/deploy_multisig.py
@@ -0,0 +1,58 @@
+from brownie import *
+
+import json
+
+def main():
+    loadConfig()
+
+    balanceBefore = acct.balance()
+    # Function Call
+    deployMultisig()
+    balanceAfter = acct.balance()
+
+    print("=============================================================")
+    print("ETH Before Balance:  ", balanceBefore)
+    print("ETH After Balance:   ", balanceAfter)
+    print("Gas Used:            ", balanceBefore - balanceAfter)
+    print("=============================================================")
+
+def loadConfig():
+    global contracts, acct
+    thisNetwork = network.show_active()
+
+    # == Load config =======================================================================================================================
+    if thisNetwork == "development":
+        acct = accounts[0]
+        configFile =  open('./scripts/uniswap/eth_testnet_contracts.json')
+    elif thisNetwork == "rinkeby":
+        acct = accounts.load("rskdeployer")
+        configFile =  open('./scripts/uniswap/eth_testnet_contracts.json')
+    elif thisNetwork == "mainnet":
+        acct = accounts.load("rskdeployer")
+        configFile =  open('./scripts/uniswap/eth_mainnet_contracts.json')
+    else:
+        raise Exception("network not supported")
+
+    # Load deployed contracts addresses
+    contracts = json.load(configFile)
+
+# == Multisig Deployment ======================================
+def deployMultisig():
+    multisigOwner1, multisigOwner2, multisigOwner3 = contracts["multisigOwner1"], contracts["multisigOwner2"], contracts["multisigOwner3"]
+    owners = [multisigOwner1, multisigOwner2, multisigOwner3]
+    requiredConf = 1
+    if network.show_active() == "mainnet":
+        requiredConf = 2
+    print("=============================================================")
+    print("Deployment Parameters")
+    print("=============================================================")
+    print("Multisig Owners:         ", owners)
+    print("Required Confirmations:  ", requiredConf)
+    print("=============================================================")
+
+    multisig = acct.deploy(MultiSigWallet, owners, requiredConf)
+    print("=============================================================")
+    print("Deployed Details")
+    print("=============================================================")
+    print("Multisig Address:        ", multisig)
+    print("=============================================================")
