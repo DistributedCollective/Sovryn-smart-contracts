97706ab2cb7acaa388308969f16f7eba20da1176
James Howlett <jameshowlett977@gmail.com>
2021-06-14 09:57:02 +0300

    updated update-lm.py

--- scripts/deployment/liquidity-mining/update-lm.py
+++ scripts/deployment/liquidity-mining/update-lm.py
@@ -76,19 +76,34 @@ def updateLMConfig():
     multisig = Contract.from_abi("MultiSig", address=contracts['multisig'], abi=MultiSigWallet.abi, owner=acct)
     lm = Contract.from_abi("LiquidityMining", address = contracts['LiquidityMiningProxy'], abi = LiquidityMining.abi, owner = acct)
 
+    # SOV/rBTC - 25k SOV
+    # ETH/rBTC - 20k SOV
+    # xUSD/rBTC - 20k SOV
+    # BNB/rBTC - 35k SOV(edited)
+
     MAX_ALLOCATION_POINT = 100000 * 1000 # 100 M
-    # 30k SOV for SOV/BTC pool
-    ALLOCATION_POINT_BTC_SOV = 30000 # (WR)BTC/SOV
-    # 25K SOV for ETH/BTC pool
-    ALLOCATION_POINT_BTC_ETH = 25000 # (WR)BTC/ETH
-    # 35K SOV for XUSD/BTC pool per week
-    ALLOCATION_POINT_BTC_XUSD = 35000 # (WR)BTC/XUSD
+    # SOV/rBTC - 25k SOV
+    ALLOCATION_POINT_BTC_SOV = 25000 # (WR)BTC/SOV
+    # ETH/rBTC - 20k SOV
+    ALLOCATION_POINT_BTC_ETH = 20000 # (WR)BTC/ETH
+    # xUSD/rBTC - 20k SOV
+    ALLOCATION_POINT_BTC_XUSD = 20000 # (WR)BTC/XUSD
     ALLOCATION_POINT_DEFAULT = 1 # (WR)BTC/USDT1 | (WR)BTC/USDT2 | (WR)BTC/DOC1 | (WR)BTC/DOC2 | (WR)BTC/BPRO1 | (WR)BTC/BPRO2 | (WR)BTC/MOC
     ALLOCATION_POINT_CONFIG_TOKEN = MAX_ALLOCATION_POINT - ALLOCATION_POINT_BTC_SOV - ALLOCATION_POINT_BTC_ETH - ALLOCATION_POINT_BTC_XUSD - ALLOCATION_POINT_DEFAULT * 7
 
     print("ALLOCATION_POINT_CONFIG_TOKEN: ", ALLOCATION_POINT_CONFIG_TOKEN)
 
-    data = lm.add.encode_input(contracts['(WR)BTC/XUSD'],ALLOCATION_POINT_BTC_XUSD,True)
+    data = lm.update.encode_input(contracts['(WR)BTC/SOV'],ALLOCATION_POINT_BTC_SOV,True)
+    tx = multisig.submitTransaction(lm.address,0,data)
+    txId = tx.events["Submission"]["transactionId"]
+    print("txid",txId)
+
+    data = lm.update.encode_input(contracts['(WR)BTC/ETH'],ALLOCATION_POINT_BTC_ETH,True)
+    tx = multisig.submitTransaction(lm.address,0,data)
+    txId = tx.events["Submission"]["transactionId"]
+    print("txid",txId)
+
+    data = lm.update.encode_input(contracts['(WR)BTC/XUSD'],ALLOCATION_POINT_BTC_XUSD,True)
     tx = multisig.submitTransaction(lm.address,0,data)
     txId = tx.events["Submission"]["transactionId"]
     print("txid",txId)
