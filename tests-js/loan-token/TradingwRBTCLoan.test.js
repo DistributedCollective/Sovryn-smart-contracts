0687d4e5c39c20a5c3a979cb511f5dcb23d54794
Santo Sinar Pandean <santo.pandean@gmail.com>
2021-07-02 18:45:58 +0700

    Fix test cases for special rebates LM

--- tests-js/loan-token/TradingwRBTCLoan.test.js
+++ tests-js/loan-token/TradingwRBTCLoan.test.js
@@ -62,13 +62,13 @@ contract("LoanTokenTrading", (accounts) => {
 
 	describe("test the loan token trading logic with SUSD test token as collateral token and the wBTC as underlying loan token. ", () => {
 		/*
-      tests margin trading sending loan tokens.
+		tests margin trading sending loan tokens.
 			process is handled by the shared function margin_trading_sending_loan_tokens
 			1. approve the transfer
 			2. send the margin trade tx
 			3. verify the trade event and balances are correct
 			4. retrieve the loan from the smart contract and make sure all values are set as expected
-    */
+    	*/
 		it("Test margin trading sending loan tokens", async () => {
 			await margin_trading_sending_loan_tokens(accounts, sovryn, loanTokenWRBTC, WRBTC, SUSD, priceFeeds, false);
 			await margin_trading_sov_reward_payment(accounts, loanTokenWRBTC, WRBTC, SUSD, SOV, FeesEvents, sovryn);
@@ -123,8 +123,8 @@ contract("LoanTokenTrading", (accounts) => {
 		it("Test margin trading sending collateral tokens with special rebates", async () => {
 			const loanSize = oneEth;
 			//  make sure there are sufficient funds on the contract
-			await loanTokenWRBTC.mintWithBTC(accounts[0], { value: loanSize.mul(new BN(6)) });
-			await loanTokenWRBTC.mintWithBTC(accounts[2], { value: loanSize.mul(new BN(6)) });
+			await loanTokenWRBTC.mintWithBTC(accounts[0], false, { value: loanSize.mul(new BN(6)).toString() });
+			await loanTokenWRBTC.mintWithBTC(accounts[2], false, { value: loanSize.mul(new BN(6)).toString() });
 			// compute the amount of collateral tokens needed
 			const collateralTokenSent = await sovryn.getRequiredCollateral(
 				WRBTC.address,
@@ -133,6 +133,7 @@ contract("LoanTokenTrading", (accounts) => {
 				new BN(50).mul(oneEth),
 				false
 			);
+
 			await SUSD.mint(accounts[0], collateralTokenSent);
 			await SUSD.mint(accounts[2], collateralTokenSent);
 			// important! WRBTC is being held by the loanToken contract itself, all other tokens are transfered directly from
@@ -141,6 +142,7 @@ contract("LoanTokenTrading", (accounts) => {
 			await SUSD.approve(loanTokenWRBTC.address, collateralTokenSent, { from: accounts[2] });
 			const leverageAmount = new BN(5).mul(oneEth);
 			const value = 0;
+
 			await margin_trading_sending_collateral_tokens(
 				accounts,
 				loanTokenWRBTC,
@@ -152,6 +154,7 @@ contract("LoanTokenTrading", (accounts) => {
 				value,
 				priceFeeds
 			);
+
 			await margin_trading_sending_collateral_tokens_sov_reward_payment_with_special_rebates(
 				accounts[2],
 				loanTokenWRBTC,
