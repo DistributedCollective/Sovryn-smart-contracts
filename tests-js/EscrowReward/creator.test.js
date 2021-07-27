44d165eecc87515e9d113b7f8b4fb0d2fc44e8e3
Franklin Richards <powerhousefrank@protonmail.com>
2021-07-27 17:58:46 +0530

    EscrowReward creator tests updated

--- tests-js/EscrowReward/creator.test.js
+++ tests-js/EscrowReward/creator.test.js
@@ -134,7 +134,7 @@ contract("Escrow Rewards (Creator Functions)", (accounts) => {
 	});
 
 	it("Creator should not be able to withdraw unless the Release Time has not passed.", async () => {
-		await escrowReward.updateReleaseTimestamp(currentTimestamp() + 100, { from: multisig });
+		await escrowReward.updateReleaseTimestamp(currentTimestamp() * 2, { from: multisig });
 
 		let oldSOVBal = await sov.balanceOf(multisig);
 		await escrowReward.withdrawTokensByMultisig(constants.ZERO_ADDRESS, { from: multisig });
