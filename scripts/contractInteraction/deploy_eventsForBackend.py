306900b23c6b38f1ebaf5b82917c122a78c34df2
Juan Ignacio Pérez Sacristán <computerphysicslab@gmail.com>
2021-07-27 14:47:11 +0200

    eventsForBackend.py using protocol.py functions

--- scripts/contractInteraction/deploy_eventsForBackend.py
+++ scripts/contractInteraction/deploy_eventsForBackend.py
@@ -29,77 +29,9 @@ def main():
     #load the contracts and acct depending on the network
     conf.loadConfig()
 
-    #call the function you want here
-    deployEventsForBackend()
-
-def replaceLoanOpenings():
-    print("replacing loan openings")
-    loanOpenings = conf.acct.deploy(LoanOpenings)
-    sovryn = Contract.from_abi("sovryn", address=conf.contracts['sovrynProtocol'], abi=interface.ISovrynBrownie.abi, owner=conf.acct)
-    data = sovryn.replaceContract.encode_input(loanOpenings.address)
-    sendWithMultisig(conf.contracts['multisig'], sovryn.address, data, conf.acct)
-
-def replaceLoanClosings():
-    sovryn = Contract.from_abi("sovryn", address=conf.contracts['sovrynProtocol'], abi=interface.ISovrynBrownie.abi, owner=conf.acct)
-
-    print('replacing loan closings base')
-    loanClosingsBase = conf.acct.deploy(LoanClosingsBase)
-    data = sovryn.replaceContract.encode_input(loanClosingsBase.address)
-    sendWithMultisig(conf.contracts['multisig'], sovryn.address, data, conf.acct)
-
-    print('replacing loan closings with')
-    loanClosingsWith = conf.acct.deploy(LoanClosingsWith)
-    data = sovryn.replaceContract.encode_input(loanClosingsWith.address)
-    sendWithMultisig(conf.contracts['multisig'], sovryn.address, data, conf.acct)
-
-def replaceLoanMaintenance():
-    print("replacing loan maintenance")
-    loanMaintenance = conf.acct.deploy(LoanMaintenance)
-    sovryn = Contract.from_abi("sovryn", address=conf.contracts['sovrynProtocol'], abi=interface.ISovrynBrownie.abi, owner=conf.acct)
-    data = sovryn.replaceContract.encode_input(loanMaintenance.address)
-    # print(data)
-    sendWithMultisig(conf.contracts['multisig'], sovryn.address, data, conf.acct)
-
-def upgradeStaking():
-    print('Deploying account:', acct)
-    print("Upgrading staking")
-
-    # Deploy the staking logic contracts
-    stakingLogic = conf.acct.deploy(Staking)
-
-    print("New staking logic address:", stakingLogic.address)
-
-    print('''
-    next steps: 
-    STEP 1: SIP
-     - create governance SIP to execute staking proxy 
-       staking.setImplementation(newStakingLogic.address)
-     - vote on SIP
-     - in 3 days after SIP executed (in 3 days ater voting) run deploy_orig_claiming ")
-
-    STEP 2: DEPLOY ORIG CLAIMING
-    - deploy_orig_claiming will deploy VestingRegistry2 contract (without exchangeAllCSOV to prevent backdoor 
-      double claiming by VestingRegistry users) and OriginInvestorsClaim contract bound to the VestingRegistry2 
-      implementation.
-    
-    STEP 3: FUND CLAIMING
-    - transfer total SOV amount to distribute to the origin investors to the OriginInvestorsClaim address
-    
-    STEP 4: LOAD REGISTRY
-    - load the registry investor -> amount: run OriginInvestorsClaim.appendInvestorsAmountsList() by chunks of 
-      250 records a time until all the list. 
-      Make sure that OriginInvestorsClaim.totalAmount() == SOV.balanceOf(OriginInvestorsClaim)
-    
-    STEP 5: run OriginInvestorsClaim.setInvestorsAmountsListInitialized() to let the contract know that 
-    it can execute the liated investors claim
-    ''')
-
-def deployEventsForBackend():
-    # -------------------------------- 1. Deploy Staking -----------------------------------------------
+    # Deployments required by eventsForBackend branch
     upgradeStaking()
-
-
-    # ---------------------------- 2. Redeploy modules which implement InterestUser -----------------------
+    
     # LoanClosingsBase
     # LoanClosingsWith
     replaceLoanClosings()
--- scripts/contractInteraction/protocol.py
+++ scripts/contractInteraction/protocol.py
@@ -233,3 +233,19 @@ def setSupportedToken(tokenAddress):
     sovryn = Contract.from_abi("sovryn", address=conf.contracts['sovrynProtocol'], abi=interface.ISovrynBrownie.abi, owner=conf.acct)
     data = sovryn.setSupportedTokens.encode_input([tokenAddress],[True])
     sendWithMultisig(conf.contracts['multisig'], sovryn.address, data, conf.acct)
+
+def upgradeStaking():
+    print('Deploying account:', conf.acct.address)
+    print("Upgrading staking")
+
+    # Deploy the staking logic contracts
+    # Commented out: Instead of redeploying logic again, use the previous logic for testnet
+    # stakingLogic = conf.acct.deploy(Staking)
+    # print("New staking logic address:", stakingLogic.address)
+    
+    # Get the proxy contract instance
+    stakingProxy = Contract.from_abi("StakingProxy", address=conf.contracts['Staking'], abi=StakingProxy.abi, owner=conf.acct)
+
+    # Register logic in Proxy
+    data = staking.setImplementation.encode_input(contracts['StakingLogic'])
+    sendWithMultisig(conf.contracts['multisig'], conf.contracts['Staking'], data, conf.acct)
