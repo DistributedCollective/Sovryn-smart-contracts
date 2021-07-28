f4f1952d070e3ffd58bef1b698a6cf46deb0eb1d
Juan Ignacio Pérez Sacristán <computerphysicslab@gmail.com>
2021-07-28 12:32:54 +0200

    npm i keccak256

--- scripts/getSelectors.js
+++ scripts/getSelectors.js
@@ -16,75 +16,71 @@ const keccak256 = require("keccak256");
 
 // Get keccak256 in 0x string format for a given content
 var keccak256_0x = function (content) {
-    return "0x" + keccak256(content).toString('hex');
-}
-
+	return "0x" + keccak256(content).toString("hex");
+};
 
 const fs = require("fs");
 
 // Parse a contract searching for function and event declarations
 var parseContract = function (fileContent) {
-    fileContent = fileContent
-        .replace(/\/\/[^\n]*\n/g, '\n') // remove comments like //
-        .replace(/\/\*[\s\S]*?\*\//g, '') // remove comments like /* */
-    ;
+	fileContent = fileContent
+		.replace(/\/\/[^\n]*\n/g, "\n") // remove comments like //
+		.replace(/\/\*[\s\S]*?\*\//g, ""); // remove comments like /* */
 
-    var signatureList = {};
-    if (searchType == 'functions') {
-        searchRegExp = new RegExp(/function [^\)]*\)/g); // focus on function declarations
-    } else if (searchType == 'events') {
-        searchRegExp = new RegExp(/event [^\)]*\)/g); // focus on event declarations
-    } else {
-        console.log("Error: Unknown searchType", searchType);
-        process.exit(1);
-    }
-    
-    while(null != (f = searchRegExp.exec(fileContent))) { // for every function or event found
-        signature = 
-            f[0]
-            .replace(/(function|event) /g, '') // remove "function " or "event " on every match
-            .replace(/([\(,])\s+/g, '$1') // remove whitespaces and newlines inmediatly after ( or ,
-            .replace(/\s+\)/g, ')') // remove whitespaces and newlines inmediatly before )
-            .replace(/\s.*?([,\)])/g, '$1') // remove var names and extra modifiers
-        ;
-        signatureList[signature] = keccak256_0x(signature);
-    }
+	var signatureList = {};
+	if (searchType == "functions") {
+		searchRegExp = new RegExp(/function [^\)]*\)/g); // focus on function declarations
+	} else if (searchType == "events") {
+		searchRegExp = new RegExp(/event [^\)]*\)/g); // focus on event declarations
+	} else {
+		console.log("Error: Unknown searchType", searchType);
+		process.exit(1);
+	}
 
-    return signatureList;
-}
+	while (null != (f = searchRegExp.exec(fileContent))) {
+		// for every function or event found
+		signature = f[0]
+			.replace(/(function|event) /g, "") // remove "function " or "event " on every match
+			.replace(/([\(,])\s+/g, "$1") // remove whitespaces and newlines inmediatly after ( or ,
+			.replace(/\s+\)/g, ")") // remove whitespaces and newlines inmediatly before )
+			.replace(/\s.*?([,\)])/g, "$1"); // remove var names and extra modifiers
+		signatureList[signature] = keccak256_0x(signature);
+	}
+
+	return signatureList;
+};
 
 // Loop through fileList and call parser on each one
 var parseContractList = function (fileList) {
-    var contractSignatures = {};
-    for(let file of fileList) {
-        // console.log("\nFile: ", file);
-        let content = fs.readFileSync(file, {encoding: 'utf8'});
-        contractSignatures[file] = parseContract(content);
-    }
+	var contractSignatures = {};
+	for (let file of fileList) {
+		// console.log("\nFile: ", file);
+		let content = fs.readFileSync(file, { encoding: "utf8" });
+		contractSignatures[file] = parseContract(content);
+	}
 
-    return contractSignatures;
-}
-
+	return contractSignatures;
+};
 
 // Open files recursively
 const glob = require("glob");
 
 var getDirectories = function (src, ext, callback) {
-    glob(src + '/**/*' + ext, callback);
+	glob(src + "/**/*" + ext, callback);
 };
 
-getDirectories(path, '.sol', function (err, res) {
-  if (err) {
-    console.log('Error', err);
-  } else {
-    contractSignatures = parseContractList(res);
-    // console.log("contractSignatures: ", contractSignatures);
-    // Loop through results and apply tabulated format to copy/past on Google Docs
-    for (const [contract, functions] of Object.entries(contractSignatures)) {
-        console.log(contract);
-        for (const [signature, selector] of Object.entries(functions)) {
-            console.log("\t" + signature + "\t" + selector.slice(0, 10) + "\t" + selector);
-        }
-    }
-  }
+getDirectories(path, ".sol", function (err, res) {
+	if (err) {
+		console.log("Error", err);
+	} else {
+		contractSignatures = parseContractList(res);
+		// console.log("contractSignatures: ", contractSignatures);
+		// Loop through results and apply tabulated format to copy/past on Google Docs
+		for (const [contract, functions] of Object.entries(contractSignatures)) {
+			console.log(contract);
+			for (const [signature, selector] of Object.entries(functions)) {
+				console.log("\t" + signature + "\t" + selector.slice(0, 10) + "\t" + selector);
+			}
+		}
+	}
 });
--- scripts/keccak256.js
+++ scripts/keccak256.js
@@ -6,18 +6,22 @@ var myArgs = process.argv.slice(2);
 // console.log('arg: ', String(myArgs[0]).split(/\r?\n/));
 
 let signatures = String(myArgs[0])
-    .replace(/;/g, '') // remove final ;
-    .split(/\r?\n/) // split lines, every line is a signature
-    .sort(function(a, b){ // sorted alphabetically
-        if(a < b) { return -1; }
-        if(a > b) { return 1; }
-        return 0;
-    })
-    .filter((item, i, ar) => ar.indexOf(item) === i) // get unique
-;
+	.replace(/;/g, "") // remove final ;
+	.split(/\r?\n/) // split lines, every line is a signature
+	.sort(function (a, b) {
+		// sorted alphabetically
+		if (a < b) {
+			return -1;
+		}
+		if (a > b) {
+			return 1;
+		}
+		return 0;
+	})
+	.filter((item, i, ar) => ar.indexOf(item) === i); // get unique
 // console.log('signatures: ', signatures);
 
-const keccak256 = require('keccak256');
-for(let s of signatures) {
-    if (s) console.log(s + "\t" + "0x" + keccak256(s).toString('hex'));
+const keccak256 = require("keccak256");
+for (let s of signatures) {
+	if (s) console.log(s + "\t" + "0x" + keccak256(s).toString("hex"));
 }
