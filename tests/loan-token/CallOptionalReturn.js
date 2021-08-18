ea9e13faf8879de1faf5818a6168e6318e3aee43
Stan <stanmarsh8867@gmail.com>
2021-08-18 22:11:42 +0530

    Formatting

--- tests/loan-token/CallOptionalReturn.js
+++ tests/loan-token/CallOptionalReturn.js
@@ -76,7 +76,7 @@ contract("CallOptionalReturn", (accounts) => {
 		await sovryn.setProtocolTokenAddress(sov.address);
 		await sovryn.setSOVTokenAddress(sov.address);
 		await sovryn.setLockedSOVAddress((await LockedSOVMockup.new(sov.address, [accounts[0]])).address);
-		
+
 		/** Deploy LoanTokenLogicBeacon */
 		let loanTokenLogicBeacon = await LoanTokenLogicBeacon.new();
 
