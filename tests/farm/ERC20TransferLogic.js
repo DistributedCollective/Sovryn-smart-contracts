dd24ac79c6d1ba625c89ce68f5fe10ed679b39a3
py-ro <pyro.sovryn@gmail.com>
2021-08-20 17:37:32 -0300

    feat(tests):add ERC20TransferLogic 'transferReward' test

--- tests/farm/ERC20TransferLogic.js
+++ tests/farm/ERC20TransferLogic.js
@@ -76,4 +76,52 @@ describe("ERC20TransferLogic", () => {
 			});
 		});
 	});
+
+	describe("getRewardTokenAddress", async () => {
+		it("should return token1 address", async () => {
+			let _tokenAddress = await transferLogic.getRewardTokenAddress();
+			expect(_tokenAddress).equal(token1.address);
+		});
+	});
+
+	describe("senderToAuthorize", async () => {
+		it("should return contract address", async () => {
+			let transferLogicAddress = await transferLogic.senderToAuthorize();
+			expect(transferLogicAddress).equal(transferLogic.address);
+		});
+	});
+
+	describe("transferReward", async () => {
+		const account1InitialBalance = new BN(100);
+		const amountToTransfer = new BN(50);
+
+		it("fails if account doesn't have reward tokens", async () => {
+			await expectRevert(transferLogic.transferReward(account2, new BN(5), false, { from: account1 }), "invalid transfer");
+		});
+
+		it("fails if account didn't approve before", async () => {
+			//send some token1 to account1 to be able to transfer
+			await token1.mint(account1, new BN(10));
+			await expectRevert(transferLogic.transferReward(account2, new BN(5), false, { from: account1 }), "invalid transfer");
+		});
+
+		it("fails if invalid address to transfer", async () => {
+			//send some token1 to account1 to be able to transfer
+			await token1.mint(account1, account1InitialBalance);
+			await token1.approve(transferLogic.address, account1InitialBalance, { from: account1 });
+			await expectRevert(transferLogic.transferReward(ZERO_ADDRESS, new BN(5), false, { from: account1 }), "invalid transfer");
+		});
+
+		it("should account1 transfer reward to account2", async () => {
+			//send some token1 to account1 to be able to transfer
+			await token1.mint(account1, account1InitialBalance);
+			await token1.approve(transferLogic.address, account1InitialBalance, { from: account1 });
+
+			await transferLogic.transferReward(account2, amountToTransfer, false, { from: account1 });
+			let account1FinalBalance = await token1.balanceOf(account1);
+			let account2FinalBalance = await token1.balanceOf(account2);
+			expect(account1FinalBalance).bignumber.equal(account1InitialBalance.sub(amountToTransfer));
+			expect(account2FinalBalance).bignumber.equal(amountToTransfer);
+		});
+	});
 });
