a9bca49490e5992bc5e2a7652a17c041a2f2e677
Stan Marsh <stanmarsh8867@gmail.com>
2021-09-15 18:38:26 +0530

    Prettier

--- contracts/governance/Staking/Staking.sol
+++ contracts/governance/Staking/Staking.sol
@@ -136,7 +136,7 @@ contract Staking is IStaking, WeightedStaking, ApprovalReceiver {
 		/// @dev Increase stake.
 		_increaseDelegateStake(delegatee, until, amount);
 		emit DelegateChanged(stakeFor, until, previousDelegatee, delegatee);
-			_updateRewards();
+		_updateRewards();
 	}
 
 	/**
@@ -179,7 +179,7 @@ contract Staking is IStaking, WeightedStaking, ApprovalReceiver {
 		_increaseDelegateStake(delegateTo, until, amount);
 
 		emit ExtendedStakingDuration(msg.sender, previousLock, until, amount);
-			_updateRewards();
+		_updateRewards();
 	}
 
 	/**
@@ -360,7 +360,7 @@ contract Staking is IStaking, WeightedStaking, ApprovalReceiver {
 		require(success, "Token transfer failed");
 
 		emit StakingWithdrawn(msg.sender, amount, until, receiver, isGovernance);
-			_updateRewards();
+		_updateRewards();
 	}
 
 	// @dev withdraws tokens for lock date 2 weeks later than given lock date
