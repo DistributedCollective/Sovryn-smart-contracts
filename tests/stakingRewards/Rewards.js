de897f970313e034f5a1b8f9652381d032cf762b
Stan Marsh <stanmarsh8867@gmail.com>
2021-09-15 16:10:12 +0530

    Removed duplicate declaration

--- tests/stakingRewards/Rewards.js
+++ tests/stakingRewards/Rewards.js
@@ -14,9 +14,6 @@ const VestingRegistryProxy = artifacts.require("VestingRegistryProxy");
 const FeeSharingProxy = artifacts.require("FeeSharingProxy");
 const Protocol = artifacts.require("sovrynProtocol");
 const BlockMockUp = artifacts.require("BlockMockUp");
-//Upgradable Vesting Registry
-const VestingRegistryLogic = artifacts.require("VestingRegistryLogic");
-const VestingRegistryProxy = artifacts.require("VestingRegistryProxy");
 
 const wei = web3.utils.toWei;
 
