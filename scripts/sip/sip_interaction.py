282330d87e3c1bbc7f0f0c244292bdb843e3c46f
Shebin John <admin@remedcu.com>
2021-11-16 13:04:15 +0530

    Added SIP-0039 Script

--- scripts/sip/sip_interaction.py
+++ scripts/sip/sip_interaction.py
@@ -20,7 +20,7 @@ def main():
 
     # Call the function you want here
 
-    createProposalSIP0037()
+    createProposalSIP0039()
 
     balanceAfter = acct.balance()
 
@@ -275,7 +275,7 @@ def createProposalSIP0035():
     description = "SIP-0035: Origins as a Subprotocol: https://github.com/DistributedCollective/SIPS/blob/04baceb/SIP-0035.md, sha256: 1f85180a76c58a2b382049e5f846c512a61b3459d193dc74c7eb3babf89bd1ba"
 
     # Create Proposal
-    createProposal(contracts['GovernorOwner'], target, value, signature, data, description)
+    # createProposal(contracts['GovernorOwner'], target, value, signature, data, description)
 
 def createProposalSIP0037():
     # Action
@@ -287,3 +287,14 @@ def createProposalSIP0037():
 
     # Create Proposal
     createProposal(contracts['GovernorOwner'], target, value, signature, data, description)
+
+def createProposalSIP0039():
+    # Action
+    target = [contracts['SOV']]
+    value = [0]
+    signature = ["symbol()"]
+    data = ["0x"]
+    description = "SIP-0039: ZERO Token Sale via Origins: https://github.com/DistributedCollective/SIPS/blob/2c21291/SIP-0039.md, sha256: 558dc035b9915e5900b0367252ba88114ea8c821b21ec0aadc5dea8b73fcd506"
+
+    # Create Proposal
+    createProposal(contracts['GovernorAdmin'], target, value, signature, data, description)
