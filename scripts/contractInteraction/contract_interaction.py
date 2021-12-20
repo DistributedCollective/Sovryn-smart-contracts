492d3972d8b947f3a8128794d31c6c69d56d4e12
Stan Marsh <stanmarsh8867@gmail.com>
2021-12-20 21:57:02 +0530

    Test Scripts

--- scripts/contractInteraction/contract_interaction.py
+++ scripts/contractInteraction/contract_interaction.py
@@ -27,79 +27,80 @@ def main():
     #load the contracts and acct depending on the network
     conf.loadConfig()
 
-    #setupMarginLoanParams(conf.contracts['SOV'], conf.contracts['iXUSD'])
-    #setupMarginLoanParams(conf.contracts['SOV'], conf.contracts['iRBTC'])
-    #setupMarginLoanParams(conf.contracts['SOV'], conf.contracts['iBPro'])
-    #setupMarginLoanParams(conf.contracts['SOV'], conf.contracts['iDOC'])
-    # setSupportedToken(conf.contracts['BNBs'])
+    def stakeTokens(sovAmount, stakeTime, acctAddress, delegateeAddress):
+        SOVtoken = Contract.from_abi("SOV", address=conf.contracts['SOV'], abi=SOV.abi, owner=acctAddress)
+        staking = Contract.from_abi("StakingTN", address=conf.contracts['StakingTN'], abi=StakingTN.abi, owner=acctAddress)
 
-    #updateLockedSOV()
+        until = int(time.time()) + int(stakeTime)
+        amount = int(sovAmount) * (10 ** 18)
 
-    #withdrawRBTCFromWatcher(20e18, conf.contracts['FastBTC'])
+        SOVtoken.approve(staking.address, amount)
+        tx = staking.stake(amount, until, acctAddress, delegateeAddress)
 
-    #this needs to be tested first. for direct trasnfer to fastbtc use the fastbtc contract address as receiver
-    #borrowRBTCWithMultisigUsingSOV(withdrawAmount, receiver)
+    def getDetails(acctAddress):
+        staking = Contract.from_abi("StakingTN", address=conf.contracts['StakingTN'], abi=StakingTN.abi, owner=acctAddress)
+        print(staking.kickoffTS())
+        print(staking.getStakes(acctAddress))
+        #print(int(len(chain) - 2 - (((time.time()- 1632148108)/32))));
+        #print(int(len(chain) - 2 - (((time.time()- 1632148108)/32))))
+        #print(staking.getPriorWeightedStake(acctAddress, int(len(chain) - 2 - (((time.time()- 1632148108)/33))), 1632148108))
+        #print(staking.getPriorWeightedStake(acctAddress, int(len(chain) - 2 - (((time.time()- 1632234508)/33))), 1632234508))
+        #print(staking.getPriorWeightedStake(acctAddress, len(chain) - 2, 1632320908))
+        #tx = staking.extendStakingDuration(1634351308, 1634437708, {"allow_revert": True})
+        # tx = staking.withdraw(1000000000000000000000, 1634437708, acctAddress)
 
-    #withdrawTokensFromWatcher(conf.contracts['XUSD'], 100e18, '0x051B89f575fCd540F0a6a5B49c75f9a83BB2Cf07')
-    #balance = getBalance(conf.contracts['XUSD'], conf.contracts['Watcher'])
-    #print(balance)
-    #withdrawTokensFromWatcher(conf.contracts['XUSD'], 100e18, conf.contracts['multisig'])
+    def getRewards(acctAddress):
+        stakingRewards = Contract.from_abi("StakingRewardsTN", address=conf.contracts['StakingRewardsProxyTN'], abi=StakingRewardsTN.abi, owner=acctAddress)
+        print(stakingRewards.getStakerCurrentReward(True, {'from': acctAddress}))
+        #print(stakingRewards.deploymentBlock())
+        #print(stakingRewards.startTime())
+        #print(stakingRewards.upgradeTime())
+        # print(stakingRewards.accumulatedRewards(acctAddress))
+        # a = stakingRewards.getClaimableReward(True, {'from': acctAddress})
+        # b = stakingRewards.accumulatedRewards(acctAddress)
+        # print(a-b)
+        # print(stakingRewards.withdrawals(acctAddress))
+        # print(stakingRewards.stakingActivity(acctAddress))
 
-    # balance = getBalance(conf.contracts['XUSD'], conf.contracts['multisig'])
-    # print(balance)
-    # if(balance > 0):
-    #     sendTokensFromMultisig(conf.contracts['XUSD'], '0x051B89f575fCd540F0a6a5B49c75f9a83BB2Cf07', balance)
+    def getImplementation():        
+        # Get the proxy contract instance
+        stakingProxy = Contract.from_abi("StakingProxyTN", address=conf.contracts['StakingTN'], abi=StakingProxyTN.abi, owner=conf.acct)
 
-    # balance = getBalance(conf.contracts['XUSD'], conf.contracts['multisig'])
-    # print(balance)
-    #sendTokensFromMultisig(conf.contracts['XUSD'], conf.contracts['Watcher'], 25e18)
+        # Register logic in Proxy
+        data = stakingProxy.getImplementation()
+        print(data)
 
-    # readPrice(conf.contracts['WRBTC'], conf.contracts['XUSD'])
-    # readPrice(conf.contracts['XUSD'], conf.contracts['WRBTC'])
+        #data = stakingProxy.getOwner()
+        #print(data)
 
-    # removeLiquidityV1toMultisigUsingWrapper(contracts["WRBTCtoSOVConverter"], 2957 * 10**18, [contracts['WRBTC'], contracts['SOV']])
-    # def removeLiquidityV1toMultisigUsingWrapper(wrapper, converter, amount, tokens, minReturn):
+    def setMaxDuration():
+        stakingRewards = Contract.from_abi("StakingRewardsTN", address=conf.contracts['StakingRewardsProxyTN'], abi=StakingRewardsTN.abi, owner=conf.acct)
+        #stakingRewards.setMaxDuration(2592000)
+        print(stakingRewards.maxDuration())
 
-    # removeLiquidityV1toMultisigUsingWrapper(conf.contracts["RBTCWrapperProxyWithoutLM"], conf.contracts["ConverterXUSD"], 2000 * 10**18, [conf.contracts['WRBTC'], conf.contracts['XUSD']], [1,1])
-    #readLiquidity()
-    #addOwnerToMultisig('0x13Be55487D37FE3C66EE7305e1e9C1ac85de75Ae')
+    #call the functions you want here
+    # stakeTokens(1000, 2246400, "0x511893483DCc1A9A98f153ec8298b63BE010A99f", "0x511893483DCc1A9A98f153ec8298b63BE010A99f")
+    # stakeTokens(1000, 4492800, "0x511893483DCc1A9A98f153ec8298b63BE010A99f", "0x511893483DCc1A9A98f153ec8298b63BE010A99f")
+    # stakeTokens(1000, 6739200, "0x511893483DCc1A9A98f153ec8298b63BE010A99f", "0x511893483DCc1A9A98f153ec8298b63BE010A99f")
+    #readStakingKickOff()
+    getRewards("0x511893483DCc1A9A98f153ec8298b63BE010A99f")
+    # getRewards("0x8517ECce55f6D11e4A60eddbB7f4090dd2aC19E7")
+    # getRewards("0xC6138eC6c65e3280e720d4E6da0FD91A061351c4")
+    # getRewards("0x49d99a6821B7569f8D409728eD1D3EF5bF1befa9")
+    # getRewards("0xBf151ed43445a093E991211d46A34f60F02a9E09")
+    # getRewards("0xC4fc1021aAFa8Dfdb02C544D264638b18673aC94")
+    # getImplementation()
+    # getDetails("0x511893483DCc1A9A98f153ec8298b63BE010A99f")
 
-    #swapTokensWithMultisig(5e18, 1, conf.contracts['WRBTC'], conf.contracts['SOV'])
+    #Bundle Deployment
+    #upgradeStaking()
+    #deployFeeSharingProxy()
+    #deployConversionFeeSharingToWRBTC()
+    #upgradeStakingRewards()
+    #updateAddresses()
+    #setMaxDuration()
 
-    #sendFromMultisig('0xD9ECB390a6a32ae651D5C614974c5570c50A5D89', 25e18)
-
-    #checkTx(560)
-    #checkTx(561)
-    #checkTx(562)
-    #confirmWithMS(564)
-    #setV1PoolOracleAddress('0xF3c356E720958100ff3F2335D288da069Aa83ce8')
-
-    #for i in range(566, 578):
-    #    checkTx(i)
-
-    
-    #for i in range(572, 578):
-    #    confirmWithMS(i)
-
-    #transferOwner('0x46EBC03EF2277308BdB106a73d11c65109C4B89B', conf.contracts['multisig'])
-    #transferOwner('0x8e75774Ef928cE730255AB594dD1b9F0a725a56b', conf.contracts['multisig'])
-
-
-    #acceptOwnershipWithMultisig('0x8e75774Ef928cE730255AB594dD1b9F0a725a56b')
-    #acceptOwnershipWithMultisig('0x73eef416cb8B63dBfcc66719F0b74Bb7baEa48Fe')
-
-
-    #removeOwnerFromMultisig('0x13Be55487D37FE3C66EE7305e1e9C1ac85de75Ae')
-
-    #swapTokens(1e18, 1, conf.contracts['swapNetwork'], conf.contracts['XUSD'], conf.contracts['MOC'])
-
-    #readOracleFromV1Converter(conf.contracts['ConverterSOV'])
-    #readOracleFromV1Converter('0x88a67a0e79e311fe93c6e2101d55d6d2ae3a7e94')
-    #setOracleOnV1Converter('0x88a67a0e79e311fe93c6e2101d55d6d2ae3a7e94', '0x756d751d09B67d8Fc98be892431926Ff3F70a991')
-
-    #sendWithMultisig(conf.contracts['multisig'], '0x3aB00BEFDd7Bfc0667DE6483D2D3b2F9A74AF2da', '0xaa9f76280000000000000000000000000000000000000000000000000000000000000000', conf.acct)
-    
-    #replaceOwnerOnMultisig(conf.contracts['multisig'], '0x13Be55487D37FE3C66EE7305e1e9C1ac85de75Ae', '0xFEe171A152C02F336021fb9E79b4fAc2304a9E7E')
-
-
-    
+    #kickOff: Monday, 20 September 2021 19:58:28 - 1639996306
+    #Upgrade: Wednesday, 22 September 2021 19:50:08 - 1632320408
+    #deploymentBlock: 2189871
+    #startTime: 1632148108
--- scripts/deployment/deploy_sov_staking_rewards_TN.py
+++ scripts/deployment/deploy_sov_staking_rewards_TN.py
@@ -35,7 +35,7 @@ def main():
     contracts = json.load(configFile)
     multisig = contracts['multisig']
     SOVAddress = contracts['SOV']
-    stakingAddress = contracts['StakingProxyTN']
+    stakingAddress = contracts['StakingTN']
 
     balanceBefore = acct.balance()
 
