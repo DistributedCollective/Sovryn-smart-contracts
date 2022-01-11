1f8a33d28304718d01502cdca34471c6e24d2633
Stan Marsh <stanmarsh8867@gmail.com>
2022-01-11 16:08:25 +0530

    Modified script to use exisitng setup

--- scripts/contractInteraction/config.py
+++ scripts/contractInteraction/config.py
@@ -34,15 +34,15 @@ def loadConfig():
             './scripts/contractInteraction/testnet_contracts.json')
     #  Adds a conditional for the set Block cron job , it will check if the
     #  script is executed as part of the setBlock cron job
-    elif thisNetwork == "rsk-testnet" and environ.get('SETBLOCK_CRON') == "1":
+    elif thisNetwork == "rsk-testnet" and environ.get('REWARDS_CRON') == "1":
         print("Running cron job")
         acct = accounts.add(
-            environ.get('SET_BLOCK'))
+            environ.get('FEE_CLAIMER'))
         configFile = open(
             './scripts/contractInteraction/testnet_contracts.json')
-    elif thisNetwork == "rsk-mainnet" and environ.get('SETBLOCK_CRON') == "1":
+    elif thisNetwork == "rsk-mainnet" and environ.get('REWARDS_CRON') == "1":
         acct = accounts.add(
-            environ.get('SET_BLOCK'))
+            environ.get('FEE_CLAIMER'))
         configFile = open(
             './scripts/contractInteraction/mainnet_contracts.json')
     elif thisNetwork == "rsk-testnet":
