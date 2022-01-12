6717c9ec7832c5bc519b2dc93346731506194175
Stan Marsh <stanmarsh8867@gmail.com>
2022-01-12 20:31:25 +0530

    Fixed config

--- scripts/contractInteraction/config.py
+++ scripts/contractInteraction/config.py
@@ -20,20 +20,7 @@ def loadConfig():
         configFile = open(
             './scripts/contractInteraction/testnet_contracts.json')
     #  Adds a conditional for the cron job , it will check id the
-    #  script is executed as part of the feeWithdrawal cron job
-    elif thisNetwork == "rsk-testnet" and environ.get('REWARDS_CRON') == "1":
-        print("Running cron job")
-        acct = accounts.add(
-            environ.get('FEE_CLAIMER'))
-        configFile = open(
-            './scripts/contractInteraction/testnet_contracts.json')
-    elif thisNetwork == "rsk-mainnet" and environ.get('REWARDS_CRON') == "1":
-        acct = accounts.add(
-            environ.get('FEE_CLAIMER'))
-        configFile = open(
-            './scripts/contractInteraction/testnet_contracts.json')
-    #  Adds a conditional for the set Block cron job , it will check if the
-    #  script is executed as part of the setBlock cron job
+    #  script is executed as part of the feeWithdrawal/setBlock cron job
     elif thisNetwork == "rsk-testnet" and environ.get('REWARDS_CRON') == "1":
         print("Running cron job")
         acct = accounts.add(
