d22e2a4432d76ba310e6ef88ac12e357a0917145
Tyrone Johnson <tjcloa@gmail.com>
2022-03-31 02:54:44 +0300

    add py task removeLendingPoolsTxLimits

---
+++ scripts/contractInteraction/tasks/removeLendingPoolsTxLimits.py
@@ -0,0 +1,21 @@
+from scripts.contractInteraction.contract_interaction import *
+
+def main():
+    print('iRBTC')
+    readTransactionLimits(conf.contracts['iRBTC'], conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro'], conf.contracts['XUSD'])
+    print('iBPro')
+    readTransactionLimits(conf.contracts['iBPro'], conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro'], conf.contracts['XUSD'])
+    print('iDOC')
+    readTransactionLimits(conf.contracts['iDOC'], conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro'], conf.contracts['XUSD'])
+    print('iUSDT')
+    readTransactionLimits(conf.contracts['iUSDT'], conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro'], conf.contracts['XUSD'])
+    print('iXUSD')
+    readTransactionLimits(conf.contracts['iXUSD'], conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro'], conf.contracts['XUSD'])
+
+    '''
+    setTransactionLimits(conf.contracts['iRBTC'], [conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro']],[0,0,0,0])
+    setTransactionLimits(conf.contracts['iBPro'], [conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro']],[0,0,0,0])
+    setTransactionLimits(conf.contracts['iDOC'], [conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro']],[0,0,0,0])
+    '''
+    # DO NOT RUN! setTransactionLimits(conf.contracts['iUSDT'], [conf.contracts['DoC'], conf.contracts['WRBTC'], conf.contracts['USDT'], conf.contracts['BPro']],[0,0,0,0])
+    
