16cf03fcd645fcdc3cb2c675c482c4eaf9ea0eef
James Howlett <jameshowlett977@gmail.com>
2022-04-16 17:26:12 +0300

    new hardhat.config.js

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,28 +5,28 @@ import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
 // An example of a deploy script that will deploy and call a simple contract.
 export default async function (hre: HardhatRuntimeEnvironment) {
-    console.log(`Running deploy script for the Greeter contract`);
+  console.log(`Running deploy script for the Greeter contract`);
 
-    // Initialize the wallet.
-    const wallet = new Wallet("");
+  // Initialize the wallet.
+  const wallet = new Wallet("");
 
-    // Create deployer object and load the artifact of the contract we want to deploy.
-    const deployer = new Deployer(hre, wallet);
-    const artifact = await deployer.loadArtifact("Staking");
+  // Create deployer object and load the artifact of the contract we want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("governance/Staking/Staking");
 
-    // Deposit some funds to L2 in order to be able to perform L2 transactions.
-    const depositAmount = ethers.utils.parseEther("0.001");
-    const depositHandle = await deployer.zkWallet.deposit({
-        to: deployer.zkWallet.address,
-        token: utils.ETH_ADDRESS,
-        amount: depositAmount,
-    });
-    // Wait until the deposit is processed on zkSync
-    await depositHandle.wait();
+  // Deposit some funds to L2 in order to be able to perform L2 transactions.
+  const depositAmount = ethers.utils.parseEther("0.001");
+  const depositHandle = await deployer.zkWallet.deposit({
+    to: deployer.zkWallet.address,
+    token: utils.ETH_ADDRESS,
+    amount: depositAmount,
+  });
+  // Wait until the deposit is processed on zkSync
+  await depositHandle.wait();
 
-    const stakingContract = await deployer.deploy(artifact);
+  const stakingContract = await deployer.deploy(artifact, []);
 
-    // Show the contract info.
-    const contractAddress = stakingContract.address;
-    console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+  // Show the contract info.
+  const contractAddress = stakingContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
 }
