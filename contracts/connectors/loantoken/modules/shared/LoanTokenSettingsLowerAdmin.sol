8f8d2d93aa5c80972a91f523c22f06282677ff55
Tyrone Johnson <tjcloa@gmail.com>
2022-04-16 14:08:05 +0300

    Revert "Temporarily revert commits that should not go into master yet"
    This reverts commit 778e41b8912ccf7c1243008147b7756f27c095cd.

--- contracts/connectors/loantoken/modules/shared/LoanTokenSettingsLowerAdmin.sol
+++ contracts/connectors/loantoken/modules/shared/LoanTokenSettingsLowerAdmin.sol
@@ -39,7 +39,7 @@ contract LoanTokenSettingsLowerAdmin is LoanTokenLogicStorage {
 	 * @return The list of function signatures (bytes4[])
 	 */
 	function getListFunctionSignatures() external pure returns (bytes4[] memory functionSignatures, bytes32 moduleName) {
-		bytes4[] memory res = new bytes4[](8);
+		bytes4[] memory res = new bytes4[](9);
 		res[0] = this.setAdmin.selector;
 		res[1] = this.setPauser.selector;
 		res[2] = this.setupLoanParams.selector;
@@ -48,6 +48,7 @@ contract LoanTokenSettingsLowerAdmin is LoanTokenLogicStorage {
 		res[5] = this.toggleFunctionPause.selector;
 		res[6] = this.setTransactionLimits.selector;
 		res[7] = this.changeLoanTokenNameAndSymbol.selector;
+		res[8] = this.pauser.selector;
 		return (res, stringToBytes32("LoanTokenSettingsLowerAdmin"));
 	}
 
--- contracts/interfaces/ILoanTokenModules.sol
+++ contracts/interfaces/ILoanTokenModules.sol
@@ -243,6 +243,8 @@ interface ILoanTokenModules {
 	) external returns (uint256 loanAmountPaid);
 
 	/** START LOAN TOKEN LOGIC STORAGE */
+	function pauser() external view returns (address);
+
 	function liquidityMiningAddress() external view returns (address);
 
 	function name() external view returns (string memory);
