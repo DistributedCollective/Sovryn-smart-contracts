4bfb3c7837ecb0dad7a8a568eb4b337334abc494
Tyrone Johnson <tjcloa@gmail.com>
2022-05-15 17:14:20 +0300

    fix bootstrap mynt decimals

--- scripts/contractInteraction/tasks/bootstrapping_MYNT_SIP_0037.py
+++ scripts/contractInteraction/tasks/bootstrapping_MYNT_SIP_0037.py
@@ -7,4 +7,4 @@ def main():
     brownie run scripts/contractInteraction/tasks/bootstrapping_MYNT_SIP_0037.py --network rsk-mainnet
     '''
     # to be executed for a year once per month according to SIP-0037 feb'22 - jan'23
-    sendMYNTFromMultisigToFeeSharingProxy(36632.144056847e18) 
+    sendMYNTFromMultisigToFeeSharingProxy(36632.144056847 * 10**18) 
