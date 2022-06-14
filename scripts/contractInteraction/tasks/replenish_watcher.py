52a8b469df79b157e689ec8d3e52b074dae15cc2
Tyrone Johnson <tjcloa@gmail.com>
2022-06-14 12:36:31 +0300

    add replenish watcher XUSD balance .py task script

--- scripts/contractInteraction/tasks/replenish_watcher.py
+++
@@ -1,9 +0,0 @@
-from scripts.contractInteraction.contract_interaction_imports  import *
-
-def main():
-    '''
-    Replenish XUSD balance of the watcher when running below threshold
-    brownie run scripts/contractInteraction/tasks/remove_lending_pools_tx_limits.py --network testnet
-    brownie run scripts/contractInteraction/tasks/remove_lending_pools_tx_limits.py --network rsk-mainnet
-    '''
-    sendTokensFromMultisig(conf.contracts['XUSD'], conf.contracts['Watcher'], 200000e18)
