f8cd85514d0e02649910000cddf4f21516e8891b
Tyrone Johnson <tjcloa@gmail.com>
2022-07-08 02:48:47 +0300

    revert to isVestingAdress with typo else tests fail

--- contracts/governance/Staking/WeightedStaking.sol
+++ contracts/governance/Staking/WeightedStaking.sol
@@ -734,7 +734,7 @@ contract WeightedStaking is Checkpoints {
         bool isVesting;
         bytes32 codeHash = _getCodeHash(stakerAddress);
         if (address(vestingRegistryLogic) != address(0)) {
-            isVesting = vestingRegistryLogic.isVestingAddress(stakerAddress);
+            isVesting = vestingRegistryLogic.isVestingAdress(stakerAddress);
         }
 
         if (isVesting) return true;
