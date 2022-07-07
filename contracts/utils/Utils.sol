ee9b4210b7bb98a0a4db1715b40f5df0e85f18db
Tyrone Johnson <tjcloa@gmail.com>
2022-07-08 01:07:30 +0300

    add SafeOwnable contract & Utils library

---
+++ contracts/utils/Utils.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.5.17;
+
+library Utils {
+    function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+}
