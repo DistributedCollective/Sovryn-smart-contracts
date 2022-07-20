96525c90a3fe09f8921ba04ce236570797f476e4
ororopickpocket <ororosovryn@protonmail.com>
2022-07-20 14:57:18 +0000

    updated scripts, including fouryearvesting list

--- scripts/contractInteraction/contract_interaction.py
+++ scripts/contractInteraction/contract_interaction.py
@@ -23,7 +23,7 @@ def main():
     #sendTokensFromMultisig(conf.contracts['XUSD'], conf.contracts['Watcher'], 300000e18)
     #sendTokensFromMultisig(conf.contracts['DoC'], conf.contracts['Watcher'], 19000e18)
     #sendFromMultisig('0xD9ECB390a6a32ae651D5C614974c5570c50A5D89', 30e18)
-    #sendFromMultisig('0xD3582f616059044c6289155433940b564bCB6476', 0.1e18)
+    #sendFromMultisig('0xD3582f616059044c6289155433940b564bCB6476', 4.9e18)
 
     #withdrawRBTCFromIWRBTC('0x9BD6759F6D9eA15D33076e55d4CBba7cf85877A7', 1.6e18)
     #sendMYNTFromMultisigToFeeSharingProxy(36632.144056847e18)
@@ -45,7 +45,7 @@ def main():
     #redeemFromAggregatorWithMS(conf.contracts['XUSDAggregatorProxy'], conf.contracts['USDT'], 1000000e18)
     #sendTokensFromMultisig(conf.contracts['USDT'], '0x4f3948816785e30c3378eD3b9F2de034e3AE2E97', 1000000e18)
     #bal = getBalance(conf.contracts['(WR)BTC/ETH'], conf.contracts['multisig'])
-    #removeLiquidityV1toMultisigUsingWrapper(conf.contracts['RBTCWrapperProxyWithoutLM'], conf.contracts['ConverterBNBs'], bal, [conf.contracts['WRBTC'], conf.contracts['BNBs']], [1,1])
+    #removeLiquidityV1toMultisigUsingWrapper(conf.contracts['RBTCWrapperProxyWithoutLM'], conf.contracts['ConverterBNBs'], 1e18, [conf.contracts['WRBTC'], conf.contracts['BNBs']], [1,1])
 
     #readMocOracleAddress()
 
@@ -57,6 +57,9 @@ def main():
     #readAllVestingContractsForAddress('0xA6575f1D5Bd6545fBd34BE05259D9d6ae60641f2')
     #getStakes('0x750C49DD9928061Df2224AA81E08Bc4a3c334874')
     #governanceWithdrawVesting('0x750C49DD9928061Df2224AA81E08Bc4a3c334874', conf.contracts['multisig'])
+
+    #addOwnerToMultisig('0x832E1bd30d037d0327F2A0447eD44FB952A9a043')
+    removeOwnerFromMultisig('0x832E1bd30d037d0327F2A0447eD44FB952A9a043')
 
     # # ---------- Transfer ownership to gov ----------
     # # core protocol
