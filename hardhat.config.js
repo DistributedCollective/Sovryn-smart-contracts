6605cd6a0fd2485d93ca1062e5655016bd654c23
Tyrone Johnson <tjcloa@gmail.com>
2022-07-31 05:34:29 +0300

    hh config .prettier format

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,17 +11,22 @@ require("hardhat-log-remover");
 require("hardhat-abi-exporter");
 require("hardhat-deploy");
 
+require("dotenv").config();
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 /// this is for use with ethers.js
 task("accounts", "Prints the list of accounts", async () => {
-	const accounts = await ethers.getSigners();
+    const accounts = await ethers.getSigners();
 
-	for (const account of accounts.address) {
-		const wallet = ethers.Wallet.fromMnemonic("test test test test test test test test test test test junk", "m/44'/60'/0'/0");
+    for (const account of accounts.address) {
+        const wallet = ethers.Wallet.fromMnemonic(
+            "test test test test test test test test test test test junk",
+            "m/44'/60'/0'/0"
+        );
 
-		console.log(account);
-	}
+        console.log(account);
+    }
 });
 
 /*
@@ -36,28 +41,28 @@ task("accounts", "Prints the list of accounts", async () => {
  *
  * Then the forking doesn't work correctly (ie. hardhat was not properly patched)
  */
-task("check-fork-patch", "Check Hardhat Fork Patch by Rainer").setAction(
-	async (taskArgs, hre) => {
-		await hre.network.provider.request({
-			method: "hardhat_reset",
-			params: [
-				{
-					forking: {
-						jsonRpcUrl: "https://mainnet.sovryn.app/rpc",
-						blockNumber: 4272658,
-					},
-				},
-			],
-		});
-		//const xusd = await IERC20.at("0xb5999795BE0EbB5bAb23144AA5FD6A02D080299F");
-        const xusd = await hre.ethers.getContractAt("ERC20", "0xb5999795BE0EbB5bAb23144AA5FD6A02D080299F");
-		const totalSupply = await xusd.totalSupply();
-		if (totalSupply.toString() === "12346114443582774719512874")
-			console.log("Hardhat mainnet forking works properly!")
-		else
-			console.log("Hardhat mainnet forking does NOT work properly!")
-	});
-
+task("check-fork-patch", "Check Hardhat Fork Patch by Rainer").setAction(async (taskArgs, hre) => {
+    await hre.network.provider.request({
+        method: "hardhat_reset",
+        params: [
+            {
+                forking: {
+                    jsonRpcUrl: "https://mainnet.sovryn.app/rpc",
+                    blockNumber: 4272658,
+                },
+            },
+        ],
+    });
+    //const xusd = await IERC20.at("0xb5999795BE0EbB5bAb23144AA5FD6A02D080299F");
+    const xusd = await hre.ethers.getContractAt(
+        "ERC20",
+        "0xb5999795BE0EbB5bAb23144AA5FD6A02D080299F"
+    );
+    const totalSupply = await xusd.totalSupply();
+    if (totalSupply.toString() === "12346114443582774719512874")
+        console.log("Hardhat mainnet forking works properly!");
+    else console.log("Hardhat mainnet forking does NOT work properly!");
+});
 
 /*task("accounts", "Prints accounts", async (_, { web3 }) => {
 	console.log();
@@ -73,83 +78,97 @@ task("check-fork-patch", "Check Hardhat Fork Patch by Rainer").setAction(
 /**/
 
 module.exports = {
-	solidity: {
-		version: "0.5.17",
-		settings: {
-			optimizer: {
-				enabled: true,
-				runs: 200,
-			},
-			outputSelection: {
-          		"*": {
-            		"*": ["storageLayout"]
-          		}
-        	},
-		},
-	},
-	abiExporter: {
-		path: "./abi",
-		clear: true,
-		flat: false,
-		only: [],
-		except: [],
-		spacing: 4,
-	},
-	contractSizer: {
-		alphaSort: false,
-		runOnCompile: false,
-		disambiguatePaths: false,
-	},
-	networks: {
-		hardhat: {
-			allowUnlimitedContractSizes: true,
-			accounts: { mnemonic: "test test test test test test test test test test test junk" },
-		},
-		localhost: {
-			allowUnlimitedContractSizes: true,
-			url: "http://localhost:8545",
-			saveDeployments: false
-		},
-		rskPublicTestnet: {
-			url: "https://public-node.testnet.rsk.co/",
-			accounts: { mnemonic: "brownie", count: 10 },
-			network_id: 31,
-			confirmations: 4,
-			gasMultiplier: 1.25,
-			//timeout: 20000, // increase if needed; 20000 is the default value
-			//allowUnlimitedContractSize, //EIP170 contrtact size restriction temporal testnet workaround
-		},
-		rskPublicMainnet: {
-			url: "https://public-node.rsk.co/",
-			network_id: 30,
-			//timeout: 20000, // increase if needed; 20000 is the default value
-		},
-		rskSovrynTestnet: {
-			url: "https://testnet.sovryn.app/rpc",
-			accounts: { mnemonic: "brownie", count: 10 },
-			network_id: 31,
-			confirmations: 4,
-			gasMultiplier: 1.25,
-			//timeout: 20000, // increase if needed; 20000 is the default value
-			//allowUnlimitedContractSize, //EIP170 contrtact size restriction temporal testnet workaround
-		},
-		rskSovrynMainnet: {
-			url: "https://mainnet.sovryn.app/rpc",
-			network_id: 30,
-			//timeout: 20000, // increase if needed; 20000 is the default value
-		},
-	},
-	paths: {
-		sources: "./contracts",
-		tests: "./tests",
-		deploy: "./deployment/deploy",
-        deployments: './deployment/deployments'
-	},
-	mocha: {
-		timeout: 800000,
-		grep: "^(?!.*; using Ganache).*",
-	},
-	namedAccounts: {
-        deployer: 0
+    solidity: {
+        version: "0.5.17",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+            outputSelection: {
+                "*": {
+                    "*": ["storageLayout"],
+                },
+            },
+        },
+    },
+    abiExporter: {
+        path: "./abi",
+        clear: true,
+        flat: false,
+        only: [],
+        except: [],
+        spacing: 4,
+    },
+    contractSizer: {
+        alphaSort: false,
+        runOnCompile: false,
+        disambiguatePaths: false,
+    },
+    networks: {
+        hardhat: {
+            allowUnlimitedContractSizes: true,
+            accounts: { mnemonic: "test test test test test test test test test test test junk" },
+        },
+        localhost: {
+            allowUnlimitedContractSizes: true,
+            url: "http://localhost:8545",
+            saveDeployments: true,
+            live: true,
+        },
+        rskPublicTestnet: {
+            url: "https://public-node.testnet.rsk.co/",
+            accounts: { mnemonic: "brownie", count: 10 },
+            network_id: 31,
+            confirmations: 4,
+            gasMultiplier: 1.25,
+            //timeout: 20000, // increase if needed; 20000 is the default value
+            //allowUnlimitedContractSize, //EIP170 contrtact size restriction temporal testnet workaround
+        },
+        rskPublicMainnet: {
+            url: "https://public-node.rsk.co/",
+            network_id: 30,
+            //timeout: 20000, // increase if needed; 20000 is the default value
+        },
+        rskSovrynTestnet: {
+            url: "https://testnet.sovryn.app/rpc",
+            accounts: { mnemonic: "brownie", count: 10 },
+            network_id: 31,
+            confirmations: 4,
+            gasMultiplier: 1.25,
+            //timeout: 20000, // increase if needed; 20000 is the default value
+            //allowUnlimitedContractSize, //EIP170 contrtact size restriction temporal testnet workaround
+        },
+        rskSovrynMainnet: {
+            url: "https://mainnet.sovryn.app/rpc",
+            network_id: 30,
+            //timeout: 20000, // increase if needed; 20000 is the default value
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./tests",
+        deploy: "./deployment/deploy",
+        deployments: "./deployment/deployments",
+    },
+    external: {
+        contracts: [
+            {
+                artifacts: "external-artifacts",
+            },
+        ],
+    },
+    typechain: {
+        outDir: "types",
+        target: "ethers-v5",
+        alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+        externalArtifacts: ["external-artifacts/*.sol/!(*.dbg.json)"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    },
+    mocha: {
+        timeout: 800000,
+        grep: "^(?!.*; using Ganache).*",
+    },
+    namedAccounts: {
+        deployer: 0,
     },
 };
