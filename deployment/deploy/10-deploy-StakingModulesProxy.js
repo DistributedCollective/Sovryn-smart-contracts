44d1f08375a94f91d21d667e594546439f604c60
Tyrone Johnson <tjcloa@gmail.com>
2022-11-12 16:07:05 +0300

    fix StakingModulesProxy hh deployment script

--- deployment/deploy/10-deploy-StakingModulesProxy.js
+++ deployment/deploy/10-deploy-StakingModulesProxy.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const { getContractNameFromScriptFileName } = require("../helpers/utils");
+const { sendWithMultisig } = require("../helpers/helpers");
 
-const deploymentName = getContractNameFromScriptFileName(path.basename(__filename));
+//const deploymentName = getContractNameFromScriptFileName(path.basename(__filename));
 const func = async function (hre) {
     const {
         deployments: { deploy, get, log },
@@ -10,12 +11,29 @@ const func = async function (hre) {
     } = hre;
     const { deployer } = await getNamedAccounts(); //await ethers.getSigners();
 
-    await deploy("StakingModulesProxy", {
-        contract: "ModulesProxy",
+    const tx = await deploy("StakingModulesProxy", {
+        //the deployment instance name
+        contract: "ModulesProxy", //the contract to deploy
         from: deployer,
         args: [],
         log: true,
+        skipIfAlreadyDeployed: true,
     });
+
+    if (tx.newlyDeployed) {
+        const stakingProxyDeployment = await get("StakingProxy");
+        const multisigDeployment = await get("MultiSigWallet");
+        if (hre.network.tags["testnet"]) {
+            //multisig is the owner
+            let stakingProxyInterface = new ethers.utils.Interface(stakingProxyDeployment.abi);
+            let data = stakingProxyInterface.encodeFunctionData("setImplementation", [tx.address]);
+            const { deployer } = await getNamedAccounts("deployer");
+            await sendWithMultisig(multisigDeployment.address, tx.address, data, deployer);
+        } else if (hre.network.tags["mainnet"]) {
+            //governance is the owner - need a SIP to register
+            // TODO: implementation ; meanwhile use brownie sip_interaction scripts to create proposal
+        }
+    }
 };
-func.tags = [deploymentName];
+func.tags = ["StakingModulesProxy"];
 module.exports = func;
