65acc8ba0fd39a2dc64d7f1bf1a3c08a77db0df5
Rainer Koirikivi <rainer@koirikivi.fi>
2023-01-23 11:43:43 +0200

    SOV-1238: remove delegateBySig

--- contracts/governance/Staking/modules/StakingGovernanceModule.sol
+++ contracts/governance/Staking/modules/StakingGovernanceModule.sol
@@ -340,88 +340,14 @@ contract StakingGovernanceModule is IFunctionsList, StakingShared, CheckpointsSh
         _delegateNext(msg.sender, delegatee, lockDate);
     }
 
-    /**
-     * @notice Delegates votes from signatory to a delegatee account.
-     * Voting with EIP-712 Signatures.
-     *
-     * Voting power can be delegated to any address, and then can be used to
-     * vote on proposals. A key benefit to users of by-signature functionality
-     * is that they can create a signed vote transaction for free, and have a
-     * trusted third-party spend rBTC(or ETH) on gas fees and write it to the
-     * blockchain for them.
-     *
-     * The third party in this scenario, submitting the SOV-holder’s signed
-     * transaction holds a voting power that is for only a single proposal.
-     * The signatory still holds the power to vote on their own behalf in
-     * the proposal if the third party has not yet published the signed
-     * transaction that was given to them.
-     *
-     * @dev The signature needs to be broken up into 3 parameters, known as
-     * v, r and s:
-     * const r = '0x' + sig.substring(2).substring(0, 64);
-     * const s = '0x' + sig.substring(2).substring(64, 128);
-     * const v = '0x' + sig.substring(2).substring(128, 130);
-     *
-     * @param delegatee The address to delegate votes to.
-     * @param lockDate The date until which the position is locked.
-     * @param nonce The contract state required to match the signature.
-     * @param expiry The time at which to expire the signature.
-     * @param v The recovery byte of the signature.
-     * @param r Half of the ECDSA signature pair.
-     * @param s Half of the ECDSA signature pair.
-     * */
-    function delegateBySig(
-        address delegatee,
-        uint256 lockDate,
-        uint256 nonce,
-        uint256 expiry,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external whenNotPaused {
-        _notSameBlockAsStakingCheckpoint(lockDate);
-
-        /**
-         * @dev The DOMAIN_SEPARATOR is a hash that uniquely identifies a
-         * smart contract. It is built from a string denoting it as an
-         * EIP712 Domain, the name of the token contract, the version,
-         * the chainId in case it changes, and the address that the
-         * contract is deployed at.
-         * */
-        bytes32 domainSeparator =
-            keccak256(
-                abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), _getChainId(), address(this))
-            );
-
-        /// @dev GovernorAlpha uses BALLOT_TYPEHASH, while Staking uses DELEGATION_TYPEHASH
-        bytes32 structHash =
-            keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, lockDate, nonce, expiry));
-
-        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
-        address signatory = ecrecover(digest, v, r, s);
-
-        /// @dev Verify address is not null and PK is not null either.
-        require(
-            RSKAddrValidator.checkPKNotZero(signatory),
-            "Staking::delegateBySig: invalid signature"
-        ); // S13
-        require(nonce == nonces[signatory]++, "Staking::delegateBySig: invalid nonce"); // S14
-        require(now <= expiry, "Staking::delegateBySig: signature expired"); // S15
-        _delegate(signatory, delegatee, lockDate);
-        // @dev delegates tokens for lock date 2 weeks later than given lock date
-        //		if message sender is a contract
-        _delegateNext(signatory, delegatee, lockDate);
-    }
-
     function getFunctionsList() external pure returns (bytes4[] memory) {
-        bytes4[] memory functionsList = new bytes4[](7);
+        bytes4[] memory functionsList = new bytes4[](6);
         functionsList[0] = this.getPriorTotalVotingPower.selector;
         functionsList[1] = this.getCurrentVotes.selector;
         functionsList[2] = this.getPriorVotes.selector;
         functionsList[3] = this.getPriorStakeByDateForDelegatee.selector;
         functionsList[4] = this.getPriorTotalStakesForDate.selector;
         functionsList[5] = this.delegate.selector;
-        functionsList[6] = this.delegateBySig.selector;
         return functionsList;
     }
 }
