a93517e49abbd5463982ce5f3024299ea4a490bc
Tyrone Johnson <tjcloa@gmail.com>
2023-02-22 15:59:45 +0300

    upgrade FourYearVestingLogic

--- deployment/deploy/1010-deploy-FourYearVestingLogic.js
+++ deployment/deploy/1010-deploy-FourYearVestingLogic.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { getContractNameFromScriptFileName } = require("../helpers/utils");
 const { ethers } = require("hardhat");
 
-const deploymentName = getContractNameFromScriptFileName(path.basename(__filename));
+// const deploymentName = getContractNameFromScriptFileName(path.basename(__filename));
 const func = async function (hre) {
     const {
         deployments: { deploy },
@@ -10,11 +10,14 @@ const func = async function (hre) {
     } = hre;
     const { deployer } = await getNamedAccounts(); //await ethers.getSigners();
 
-    await deploy(deploymentName, {
+    // the logic is used aking Clones design pattern
+    // actual vestings are passed the logic at creation and delegate calls to it
+    // therefore using it as a library
+    const tx = await deploy("FourYearVestingLogic", {
         from: deployer,
         args: [],
         log: true,
     });
 };
-func.tags = [deploymentName];
+func.tags = ["FourYearVestingLogic"];
 module.exports = func;
