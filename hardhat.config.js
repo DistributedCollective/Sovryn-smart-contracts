cd9d0d25386d0fffcd744f873b409c3bec19fc0f
Tyrone Johnson <tjcloa@gmail.com>
2023-03-04 00:33:39 +0300

    hh config clean up

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,22 +26,6 @@ require("./hardhat/tasks/tasks");
 
 require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-/// this is for use with ethers.js
-/*task("accounts", "Prints the list of accounts", async () => {
-    const accounts = await ethers.getSigners();
-
-    for (const account of accounts.address) {
-        const wallet = ethers.Wallet.fromMnemonic(
-            "test test test test test test test test test test test junk",
-            "m/44'/60'/0'/0"
-        );
-
-        console.log(account);
-    }
-});
-
 const testnetAccounts = process.env.TESTNET_DEPLOYER_PRIVATE_KEY
     ? [process.env.TESTNET_DEPLOYER_PRIVATE_KEY, process.env.TESTNET_SIGNER_PRIVATE_KEY]
     : [];
