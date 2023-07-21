e30bfe20d4a5ebca001e860f13f35643377f63f2
Tyrone Johnson <tjcloa@gmail.com>
2023-07-22 02:52:56 +0300

    fuzz staking withdraw WIP - requires split

--- tests-foundry/staking/StakingStake.t.sol
+++ tests-foundry/staking/StakingStake.t.sol
@@ -6,7 +6,12 @@ import "forge-std/Test.sol";
 import { IStaking } from "./interfaces/IStaking.sol";
 import { IStakingProxy } from "./interfaces/IStaking.sol";
 import { IStakingModulesProxy } from "./interfaces/IStaking.sol";
+import {
+    IFeeSharingCollector,
+    IFeeSharingCollectorProxy
+} from "./interfaces/IFeeSharingCollector.sol";
 import { IERC20 } from "./interfaces/ITokens.sol";
+import { DummyMockContract } from "./mocks/DummyMockContract.sol";
 
 contract StakingFuzzTest is Test {
     // Test Variables
@@ -19,9 +24,12 @@ contract StakingFuzzTest is Test {
     uint256 private maxDuration;
     IStaking private staking;
     IERC20 private sov;
+    IFeeSharingCollector private feeSharingCollector;
+    DummyMockContract private dummyProtocol; // general mock contract, used for foundry mockCall
 
     uint96 constant MAX_96 = 2**96 - 1;
     uint256 constant MAX_256 = 2**256 - 1;
+    uint32 constant MAX_32 = 2**32 - 1;
 
     event TokensStaked(
         address indexed staker,
@@ -44,12 +52,23 @@ contract StakingFuzzTest is Test {
         address indexed toDelegate
     );
 
+    event StakingWithdrawn(
+        address indexed staker,
+        uint256 amount,
+        uint256 until,
+        address indexed receiver,
+        bool isGovernance
+    );
+
     // Test Setup
     function setUp() public virtual {
         // Initialize test variables
         sov = IERC20(
             deployCode("TestToken.sol", abi.encode("Sovryn", "SOV", 18, 100_000_000 ether))
         );
+
+        dummyProtocol = new DummyMockContract();
+
         // console.log("SOV total supply:", sov.totalSupply());
 
         address[7] memory stakingModules =
@@ -79,6 +98,21 @@ contract StakingFuzzTest is Test {
         maxDuration = staking.MAX_DURATION();
         // console.log("kickoffTS: %s", kickoffTS);
         // console.log("kickoffTS + 2 weeks: %s", kickoffTS + 2 weeks);
+
+        // Deploy FeeSharingCollector
+        IFeeSharingCollectorProxy feeSharingCollectorProxy =
+            IFeeSharingCollectorProxy(
+                deployCode(
+                    "FeeSharingCollectorProxy.sol",
+                    abi.encode(address(dummyProtocol), address(staking))
+                )
+            );
+        feeSharingCollector = IFeeSharingCollector(deployCode("FeeSharingCollector.sol"));
+        feeSharingCollectorProxy.setImplementation(address(feeSharingCollector));
+        feeSharingCollector = IFeeSharingCollector(address(feeSharingCollectorProxy));
+
+        // Set FeeSharingCollectorProxy as the fee sharing address in the staking contract
+        staking.setFeeSharing(address(feeSharingCollector));
 
         amount = 1000;
         user = address(1);
@@ -120,7 +154,7 @@ contract StakingFuzzTest is Test {
             _randomLockTimestamp = bound(
                 _randomLockTimestamp,
                 kickoffTS + 2 weeks,
-                block.timestamp + 60 + staking.MAX_DURATION()
+                block.timestamp + 60 + maxDuration
             );
             // shifting timestamp to make it invalid if fuzz is a valid timestamp
             uint256 calculatedExpectedTimestamp =
@@ -349,7 +383,7 @@ contract StakingFuzzTest is Test {
         _randomLockTimestamp = bound(
             _randomLockTimestamp,
             kickoffTS + 2 weeks,
-            block.timestamp + staking.MAX_DURATION()
+            block.timestamp + maxDuration
         );
         _amount = bound(_amount, 0, sov.totalSupply());
         amount = _amount;
@@ -409,7 +443,7 @@ contract StakingFuzzTest is Test {
         _randomLockTimestamp = bound(
             _randomLockTimestamp,
             kickoffTS + 2 weeks,
-            block.timestamp + staking.MAX_DURATION()
+            block.timestamp + maxDuration
         );*/
         // _amount = bound(_amount, 0, sov.totalSupply());
         deal(address(sov), user, amount);
@@ -515,11 +549,7 @@ contract StakingFuzzTest is Test {
 
         mineBlocks(1);
 
-        _extendUntil = bound(
-            _extendUntil,
-            kickoffTS + 2 weeks,
-            block.timestamp + staking.MAX_DURATION()
-        );
+        _extendUntil = bound(_extendUntil, kickoffTS + 2 weeks, block.timestamp + maxDuration);
 
         staking.stake(uint96(amount), lockedDate, address(0), address(0));
 
@@ -560,11 +590,7 @@ contract StakingFuzzTest is Test {
 
         uint256 blockBefore = block.number;
         mineBlocks(1);
-        _extendUntil = bound(
-            _extendUntil,
-            kickoffTS + 2 weeks,
-            block.timestamp + staking.MAX_DURATION()
-        );
+        _extendUntil = bound(_extendUntil, kickoffTS + 2 weeks, block.timestamp + maxDuration);
         uint256 latest = staking.timestampToLockDate(block.timestamp + maxDuration);
         uint256 expectedExtendUntil = staking.timestampToLockDate(_extendUntil);
         if (_extendUntil > latest) {
@@ -748,6 +774,8 @@ contract StakingFuzzTest is Test {
             checkpoint.stake == amount + _increaseAmount96,
             "Unexpected delegate staking checkpoint stake"
         );
+
+        vm.stopPrank();
     }
 
     // STAKE BY SCHEDULE TESTS
@@ -904,5 +932,211 @@ contract StakingFuzzTest is Test {
             emit DelegateChanged(user, lockedDate, address(0), delegatee);
             */
         }
+        vm.stopPrank();
+    }
+
+    function testFuzz_Withdraw(uint256 _withdrawTS, uint256 _withdrawAmount) external {
+        // _amount = bound(_amount, 0, sov.totalSupply());
+        _withdrawAmount = bound(_withdrawAmount, 0, MAX_96);
+        // deal(address(sov), user, _withdrawAmount);
+        //deal(address(sov), user, amount);
+        //mineBlocks(1);
+        // set staking to have more than on stake
+
+        uint256 timestampToLockDateBefore = staking.timestampToLockDate(block.timestamp);
+        uint256 timestampToLockDate = staking.timestampToLockDate(block.timestamp + 365 days);
+
+        deal(address(sov), user2, amount * 2);
+        vm.startPrank(user2);
+        sov.approve(address(staking), amount * 2);
+        staking.stake(uint96(amount * 2), 365 days, address(0), address(0));
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        sov.approve(address(staking), amount);
+        staking.stake(uint96(amount), 365 days, address(0), address(0));
+        //sov.approve(address(staking), amount);
+        mineBlocks(1);
+        // uint256 timestampToLockDate = staking.timestampToLockDate(_withdrawTS);
+
+        emit log_named_uint("MAX_32", MAX_32);
+        emit log_named_uint("MAX_32 * 30", uint256(MAX_32) * 30);
+        emit log_named_uint(
+            "MAX_32 * 30 + block.timestamp",
+            uint256(MAX_32) * 30 + block.timestamp
+        );
+        uint256 userBalanceBefore = sov.balanceOf(user);
+        uint256 stakingBalanceBefore = sov.balanceOf(address(staking));
+
+        // block.number * _timePerBlockInSeconds < MAX_256
+        _withdrawTS = bound(
+            _withdrawTS,
+            block.timestamp,
+            (uint256(MAX_32) - block.timestamp) * 30 + block.timestamp
+        );
+
+        emit log_named_uint("block.timestamp", block.timestamp);
+        emit log_named_uint("_withdrawTS", _withdrawTS);
+        emit log_named_uint(
+            "(_withdrawTS - block.timestamp) / 30",
+            (_withdrawTS - block.timestamp) / 30
+        );
+        mineBlocks((_withdrawTS - block.timestamp) / 30);
+        //@todo set withdrawable timestamp here
+        _withdrawAmount = bound(_withdrawAmount, 1, 1000);
+        //_withdrawTS = bound(_withdrawTS, block.timestamp + 365, MAX_256);
+        emit log_named_uint("block.number", block.number);
+        emit log_named_uint("block.timestamp", block.timestamp);
+        emit log_named_uint("timestampToLockDate", timestampToLockDate);
+        (uint96 expectedWithdrawAmount, uint96 expectedSlashAmount) =
+            staking.getWithdrawAmounts(uint96(_withdrawAmount), timestampToLockDate);
+
+        vm.expectEmit();
+        emit StakingWithdrawn({
+            staker: user,
+            amount: expectedWithdrawAmount,
+            until: timestampToLockDate,
+            receiver: user,
+            isGovernance: false
+        });
+        emit log_named_uint("expectedWithdrawAmount", expectedWithdrawAmount);
+        emit log_named_uint("expectedSlashAmount", expectedSlashAmount);
+        staking.withdraw(uint96(_withdrawAmount), timestampToLockDate, address(0));
+        return;
+        // check for known restrictions failures
+        if (
+            _withdrawAmount == 0 ||
+            _withdrawAmount > amount ||
+            _withdrawAmount > staking.getPriorUserStakeByDate(user, _withdrawTS, block.number - 1)
+        ) {
+            emit log_named_uint("_withdrawTS", _withdrawTS);
+            emit log_named_uint("kickoffTS + 4 weeks", kickoffTS + 4 weeks);
+            emit log_named_uint("_withdrawAmount", _withdrawAmount);
+            emit log_named_uint(
+                "staking.getPriorUserStakeByDate(user, _withdrawTS, block.number - 1)",
+                staking.getPriorUserStakeByDate(user, _withdrawTS, block.number - 1)
+            );
+
+            // fast forward to the _withdrawTS
+            vm.expectRevert();
+            staking.withdraw(uint96(_withdrawAmount), _withdrawTS, address(0));
+            return;
+        }
+
+        uint256 withdrawTimestampToLockDate = staking.timestampToLockDate(_withdrawTS);
+
+        // get expected values
+        (expectedWithdrawAmount, expectedSlashAmount) = staking.getWithdrawAmounts(
+            uint96(_withdrawAmount),
+            _withdrawTS
+        );
+
+        uint256 blockAfter = block.number;
+        // uint256 blockBefore == blockAfter - 1;
+
+        vm.expectEmit();
+        emit StakingWithdrawn({
+            staker: address(this),
+            amount: _withdrawAmount,
+            until: timestampToLockDate,
+            receiver: user,
+            isGovernance: false
+        });
+
+        vm.mockCall(
+            address(dummyProtocol),
+            abi.encodeWithSelector(dummyProtocol.wrbtcToken.selector),
+            abi.encode(address(4))
+        );
+
+        staking.withdraw(uint96(amount), _withdrawTS, address(0));
+
+        mineBlocks(1);
+
+        // Get final balances
+        uint256 userBalanceAfter = sov.balanceOf(user);
+        uint256 stakingBalanceAfter = sov.balanceOf(address(staking));
+
+        // Check balances
+        assertEq(userBalanceBefore - userBalanceAfter, expectedWithdrawAmount);
+        assertEq(
+            stakingBalanceAfter - stakingBalanceBefore,
+            expectedWithdrawAmount + expectedSlashAmount
+        );
+
+        // Check getPriorUserStakeByDate
+
+        uint256 priorUserStakeBefore =
+            staking.getPriorUserStakeByDate(user, withdrawTimestampToLockDate, blockAfter - 1);
+        uint256 priorUserStakeAfter =
+            staking.getPriorUserStakeByDate(user, withdrawTimestampToLockDate, blockAfter);
+
+        assertEq(priorUserStakeAfter - priorUserStakeBefore, _withdrawAmount);
+
+        // check user2 staking unchanged
+        (uint256[] memory dates, uint96[] memory stakes) = staking.getStakes(user2);
+        assertEq(dates.length, 1);
+        assertEq(stakes.length, 1);
+        assertEq(dates[0], timestampToLockDateBefore);
+        assertEq(stakes[0], 1000 ether);
+
+        /*
+        // Extended validation
+        // _increaseDailyStake
+        uint32 numTotalStakingCheckpoints = staking.numTotalStakingCheckpoints(lockedTS);
+        assertEq(numTotalStakingCheckpoints, 3, "Unexpected number of total staking checkpoints");
+
+        IStaking.Checkpoint memory checkpoint = staking.totalStakingCheckpoints(lockedTS, 0);
+        assertTrue(
+            checkpoint.fromBlock == tx1.blockNumber,
+            "Unexpected total staking checkpoint fromBlock"
+        );
+        assertTrue(checkpoint.stake == amount, "Unexpected total staking checkpoint stake");
+
+        checkpoint = staking.totalStakingCheckpoints(lockedTS, 1);
+        assertTrue(
+            checkpoint.fromBlock == tx2.blockNumber,
+            "Unexpected total staking checkpoint fromBlock"
+        );
+        assertTrue(checkpoint.stake == halfAmount, "Unexpected total staking checkpoint stake");
+
+        // _writeUserCheckpoint
+        uint256 numUserCheckpoints = staking.numUserStakingCheckpoints(address(root), lockedTS);
+        assertTrue(numUserCheckpoints == 2, "Unexpected number of user staking checkpoints");
+
+        checkpoint = staking.userStakingCheckpoints(address(root), lockedTS, 0);
+        assertTrue(
+            checkpoint.fromBlock == tx1.blockNumber,
+            "Unexpected user staking checkpoint fromBlock"
+        );
+        assertTrue(checkpoint.stake == amount, "Unexpected user staking checkpoint stake");
+
+        checkpoint = staking.userStakingCheckpoints(address(root), lockedTS, 1);
+        assertTrue(
+            checkpoint.fromBlock == tx2.blockNumber,
+            "Unexpected user staking checkpoint fromBlock"
+        );
+        assertTrue(checkpoint.stake == halfAmount, "Unexpected user staking checkpoint stake");
+
+        // _decreaseDelegateStake
+        uint32 numDelegateStakingCheckpoints =
+            staking.numDelegateStakingCheckpoints(address(root), lockedTS);
+        checkpoint = staking.delegateStakingCheckpoints(
+            address(root),
+            lockedTS,
+            numDelegateStakingCheckpoints - 1
+        );
+        assertTrue(
+            checkpoint.fromBlock == tx2.blockNumber,
+            "Unexpected delegate staking checkpoint fromBlock"
+        );
+        assertTrue(checkpoint.stake == halfAmount, "Unexpected delegate staking checkpoint stake");
+        assertTrue(
+            numDelegateStakingCheckpoints == 2,
+            "Unexpected number of delegate staking checkpoints"
+        );
+        */
+        vm.clearMockedCalls();
+        vm.stopPrank();
     }
 }
