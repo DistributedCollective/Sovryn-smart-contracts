ebd3c0432dc51ba2bfef26d0abf8b95c30b69f92
Ubuntu <ubuntu@ip-10-0-12-30.us-east-2.compute.internal>
2023-10-17 02:22:18 +0000

    adding sip arguments for SIP0047 - under review

--- hardhat/tasks/sips/args/sipArgs.js
+++ hardhat/tasks/sips/args/sipArgs.js
@@ -52,6 +52,40 @@ const sampleGovernorAdminSIP = async (hre) => {
     return { args, governor: "GovernorAdmin" };
 };
 
+const getArgsSip0047 = async (hre) => {
+    const {
+        ethers,
+        deployments: { get },
+    } = hre;
+    const abiCoder = new ethers.utils.AbiCoder();
+    const multisigDeployment = await get("MultiSigWallet");
+    const staking = await get("Staking");
+    const multisigAddress = multisigDeployment.address;
+    const stakingAddress = staking.address;
+    let guardianAddress;
+    if (network.tags.testnet) {
+        guardianAddress = "0xE79232Db7d096e781647405B28398121AEA08536".toLowerCase();
+    } else if (network.tags.mainnet) {
+        guardianAddress = "0xDd8e07A57560AdA0A2D84a96c457a5e6DDD488b7".toLowerCase();
+    } else {
+        logger.error("Unknown network");
+        process.exit(1);
+    }
+    const args = {
+        targets: [stakingAddress, stakingAddress],
+        values: [0, 0],
+        signatures: ["addPauser(address)", "removePauser(address)"],
+        data: [
+            abiCoder.encode(["address"], [guardianAddress]),
+            abiCoder.encode(["address"], [multisigAddress]),
+        ],
+        description:
+            // WARNING: the sha256sum value shown is for the current state of the document which will have to be changed with the guardian signer addresses
+            "SIP-0047: Changing Staking contract pauser from Exchequer to Guardian, Details: https://github.com/DistributedCollective/SIPS/blob/251a84d39cce5fd91c219ef5dbb01043b2338458/SIP-0047.md, sha256: a04970554a038bc3d76652828ec0e5e9482287be73aa558d811117550facf3a1",
+    };
+    return { args, governor: "GovernorOwner" };
+};
+
 const getArgsSip0049 = async (hre) => {
     const {
         ethers,
@@ -258,6 +292,7 @@ const getArgsSip0065 = async (hre) => {
 module.exports = {
     sampleGovernorAdminSIP,
     sampleGovernorOwnerSIP,
+    getArgsSip0047,
     getArgsSip0058,
     getArgsSip0049,
     getArgsSip0063,
