388e0ca093d44f45734fd5720200d6b27134a26f
Santo Sinar Pandean <santo.pandean@gmail.com>
2023-11-06 08:15:28 +0700

    ran prettier

--- interfaces/IMyntReserve.sol
+++ interfaces/IMyntReserve.sol
@@ -1,5 +1,9 @@
 pragma solidity 0.5.17;
 
 contract IMyntReserve {
-    function transfer(address _token, address _to, uint256 _value) external;
+    function transfer(
+        address _token,
+        address _to,
+        uint256 _value
+    ) external;
 }
