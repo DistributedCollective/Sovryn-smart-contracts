46c32629948da119121474914cf7e396ff77a3b8
Tyrone Johnson <tjcloa@gmail.com>
2024-02-28 14:41:09 +0300

    enhance bridge:allowToken task

--- hardhat/tasks/bridge.js
+++ hardhat/tasks/bridge.js
@@ -5,40 +5,61 @@ const { sendWithMultisig } = require("../../deployment/helpers/helpers");
 const { Percent } = require("@uniswap/sdk-core");
 const { nearestUsableTick, Position } = require("@uniswap/v3-sdk");
 
-task("allowToken", "Allow token")
+task("bridge:allowToken", "Whitelist token on a bridge")
+    .addPositionalParam("token", "Address or deployment name (e.g. DLLR) of a token to whitelist")
+    .addOptionalParam("bridge", "Bridge contract to set allowance to", "BridgeRSK")
     .addOptionalParam("signer", "Signer name: 'signer' or 'deployer'", "deployer")
-    .addParam("token", "Address of token")
-    .setAction(async ({ signer, token }, hre) => {
+    .setAction(async ({ signer, token, bridge }, hre) => {
         const {
-            deployments: { get },
+            deployments: { get, getArtifact },
             ethers,
         } = hre;
 
-        const allowTokensDeploymentAbi = (await deployments.getArtifact("AllowTokens")).abi;
-
-        const bridgeContract = await ethers.getContract("BridgeRSK");
-
+        const tokenAddress = ethers.utils.isAddress(token) ? token : (await get(token)).address;
+        const signerAcc = (await hre.getNamedAccounts())[signer];
+        const allowTokensArtifact = await getArtifact("AllowTokens");
+        const bridgeContract = await ethers.getContract(bridge);
         const allowTokensAddress = await bridgeContract.allowTokens();
-
         const allowTokensContract = await ethers.getContractAt(
-            allowTokensDeploymentAbi,
+            allowTokensArtifact.abi,
             allowTokensAddress
         );
 
         const multisigAddress = await allowTokensContract.owner();
         const multisigContract = await ethers.getContractAt("MultiSigWallet", multisigAddress);
 
-        const allowTokensInterface = new ethers.utils.Interface(allowTokensDeploymentAbi);
-        let data = allowTokensInterface.encodeFunctionData("addAllowedToken", [token]);
+        const allowTokensInterface = new ethers.utils.Interface(allowTokensArtifact.abi);
+        let data = allowTokensInterface.encodeFunctionData("addAllowedToken", [tokenAddress]);
 
-        const tx = await multisigContract.populateTransaction.submitTransaction(
-            allowTokensAddress,
-            0,
-            data
-        );
+        if (await multisigContract.isOwner(signerAcc)) {
+            await sendWithMultisig(
+                multisigContract.address,
+                allowTokensContract.address,
+                data,
+                signerAcc
+            );
+        } else {
+            logger.warn(
+                `The wallet ${signerAcc} is not an owner of the multisig ${multisigAddress}. Only multisig owners can whitelist tokens on a bridge`
+            );
+            logger.warn("Populating multisig tx...");
 
-        delete tx.from;
-        logger.warning("==================== populated tx start ====================");
-        logger.info(tx);
-        logger.warning("==================== populated tx end   =================");
+            const gasEstimated = (
+                await multisigContract.estimateGas.submitTransaction(allowTokensAddress, 0, data)
+            ).toNumber();
+
+            const unsignedTx = await multisigContract.populateTransaction.submitTransaction(
+                allowTokensAddress,
+                0,
+                data,
+                {
+                    gasLimit: Math.round(gasEstimated * 1.3),
+                }
+            );
+
+            delete unsignedTx.from;
+            logger.warning("==================== populated tx start ====================");
+            logger.info(unsignedTx);
+            logger.warning("==================== populated tx end   =================");
+        }
     });
