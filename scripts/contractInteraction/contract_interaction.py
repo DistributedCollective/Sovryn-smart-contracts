86abd5a0e50fa320324939e65ef559244127bde3
Tyrone Johnson <tjcloa@gmail.com>
2024-05-28 16:36:57 +0300

    add brownie .py scripts add/remove OwnerToAnyMultisig

--- scripts/contractInteraction/contract_interaction.py
+++ scripts/contractInteraction/contract_interaction.py
@@ -32,6 +32,8 @@ def main():
     
     ##################################
 
+    #addOwnerToAnyMultisig('0x37a706259F5201c03f6Cb556A960f30F86842D01','0x13Be55487D37FE3C66EE7305e1e9C1ac85de75Ae') # BF
+    #removeOwnerFromAnyMultisig('0x37a706259F5201c03f6Cb556A960f30F86842D01','0x9E0816a71B53ca67201a5088df960fE90910DE55') # BF
     #cancelTeamVestingsOfAccount('0x673b37941AB527e0EEe13C1Ff09298ef1911d7D6', 1703845695)
     #cancelTeamVestingsOfAccount('0x56B00ca0a274fB53449fBF2DB0253B809E364975')
 
