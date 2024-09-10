641671730a73bc940c9686797c5cc5d88bd908e4
cwsnt <undefined@udefined.com>
2024-09-10 08:25:53 +0700

    ran prettier

--- deployment/deploy/config/data.js
+++ deployment/deploy/config/data.js
@@ -2,7 +2,7 @@ const testnetDeploymentConfigParams = {
     multisigOwners: [
         "0x13Be55487D37FE3C66EE7305e1e9C1ac85de75Ae",
         "0xCF311E7375083b9513566a47B9f3e93F1FcdCfBF",
-        "0x8C9143221F2b72Fcef391893c3a02Cf0fE84f50b"
+        "0x8C9143221F2b72Fcef391893c3a02Cf0fE84f50b",
     ],
     required: 2,
 };
