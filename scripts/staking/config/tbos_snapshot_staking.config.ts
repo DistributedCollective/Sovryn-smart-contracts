83eeca4cbdd55fbde8784dbe72920cc4d548f5f9
cwsnt <undefined@udefined.com>
2025-05-05 21:39:43 +0700

    init: tbos snapshot script

---
+++ scripts/staking/config/tbos_snapshot_staking.config.ts
@@ -0,0 +1,5 @@
+export const TBOS_SNAPSHOT_STAKING_CONFIG = {
+  averageBlockTime: 15,
+  stakingAddress: "0xc17C6462cEAFE9A8819258c6bA168BEF5544Fc21",
+  rpcUrl: "https://rpc.gobob.xyz/",
+} as const;
